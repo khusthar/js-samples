91c1ee0f1a2d3be1e7a34d8799e5d4d70a3bcbfb
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:39:54 +0000

    chore: sync landing-page-visualize-data [skip-ci]

--- src/index.ts
+++ src/index.ts
@@ -16,63 +16,48 @@
 /* eslint-disable no-undef, @typescript-eslint/no-unused-vars, no-unused-vars */
 import "./style.css";
 
+let map: google.maps.Map;
+
 function initMap(): void {
-  const map = new google.maps.Map(
-    document.getElementById("map") as HTMLElement,
-    {
-      zoom: 3,
-      center: { lat: -28.024, lng: 140.887 },
-      scrollwheel: false,
-      zoomControl: false,
-      scaleControl: false,
-      streetViewControl: false,
-    }
-  );
-
-  // Create an array of alphabetical characters used to label the markers.
-  const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-  // Add some markers to the map.
-  // Note: The code uses the JavaScript Array.prototype.map() method to
-  // create an array of markers based on a given "locations" array.
-  // The map() method here has nothing to do with the Google Maps API.
-  const markers = locations.map((location, i) => {
-    return new google.maps.Marker({
-      position: location,
-      label: labels[i % labels.length],
-    });
+  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+    zoom: 2,
+    center: { lat: -33.865427, lng: 151.196123 },
+    mapTypeId: "terrain",
+    zoomControl: false,
+    scaleControl: false,
+    streetViewControl: false,
+    gestureHandling: "cooperative",
   });
 
-  // Add a marker clusterer to manage the markers.
-  // @ts-ignore
-  new MarkerClusterer(map, markers, {
-    imagePath:
-      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+  // Create a <script> tag and set the USGS URL as the source.
+  const script = document.createElement("script");
+
+  // This example uses a local copy of the GeoJSON stored at
+  // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+  script.src =
+    "https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js";
+  document.getElementsByTagName("head")[0].appendChild(script);
+
+  map.data.setStyle((feature) => {
+    const magnitude = feature.getProperty("mag");
+    return {
+      icon: getCircle(magnitude),
+    };
   });
 }
-const locations = [
-  { lat: -31.56391, lng: 147.154312 },
-  { lat: -33.718234, lng: 150.363181 },
-  { lat: -33.727111, lng: 150.371124 },
-  { lat: -33.848588, lng: 151.209834 },
-  { lat: -33.851702, lng: 151.216968 },
-  { lat: -34.671264, lng: 150.863657 },
-  { lat: -35.304724, lng: 148.662905 },
-  { lat: -36.817685, lng: 175.699196 },
-  { lat: -36.828611, lng: 175.790222 },
-  { lat: -37.75, lng: 145.116667 },
-  { lat: -37.759859, lng: 145.128708 },
-  { lat: -37.765015, lng: 145.133858 },
-  { lat: -37.770104, lng: 145.143299 },
-  { lat: -37.7737, lng: 145.145187 },
-  { lat: -37.774785, lng: 145.137978 },
-  { lat: -37.819616, lng: 144.968119 },
-  { lat: -38.330766, lng: 144.695692 },
-  { lat: -39.927193, lng: 175.053218 },
-  { lat: -41.330162, lng: 174.865694 },
-  { lat: -42.734358, lng: 147.439506 },
-  { lat: -42.734358, lng: 147.501315 },
-  { lat: -42.735258, lng: 147.438 },
-  { lat: -43.999792, lng: 170.463352 },
-];
-export { initMap };
+
+function getCircle(magnitude: number) {
+  return {
+    path: google.maps.SymbolPath.CIRCLE,
+    fillColor: "red",
+    fillOpacity: 0.2,
+    scale: Math.pow(2, magnitude) / 2,
+    strokeColor: "white",
+    strokeWeight: 0.5,
+  };
+}
+
+function eqfeed_callback(results: any) {
+  map.data.addGeoJson(results);
+}
+export { initMap, eqfeed_callback };
