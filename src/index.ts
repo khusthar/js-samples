e41d035f041dd6819dbf8e7ee6af0efe7ee2e53e
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:35:22 +0000

    chore: sync directions-draggable [skip-ci]

--- src/index.ts
+++ src/index.ts
@@ -17,129 +17,75 @@
 import "./style.css";
 
 function initMap(): void {
-  const markerArray: google.maps.Marker[] = [];
-
-  // Instantiate a directions service.
-  const directionsService = new google.maps.DirectionsService();
-
-  // Create a map and center it on Manhattan.
   const map = new google.maps.Map(
     document.getElementById("map") as HTMLElement,
     {
-      zoom: 13,
-      center: { lat: 40.771, lng: -73.974 },
+      zoom: 4,
+      center: { lat: -24.345, lng: 134.46 }, // Australia.
     }
   );
 
-  // Create a renderer for directions and bind it to the map.
-  const directionsRenderer = new google.maps.DirectionsRenderer({ map: map });
+  const directionsService = new google.maps.DirectionsService();
+  const directionsRenderer = new google.maps.DirectionsRenderer({
+    draggable: true,
+    map,
+    panel: document.getElementById("right-panel") as HTMLElement,
+  });
 
-  // Instantiate an info window to hold step text.
-  const stepDisplay = new google.maps.InfoWindow();
+  directionsRenderer.addListener("directions_changed", () => {
+    computeTotalDistance(directionsRenderer.getDirections()!);
+  });
 
-  // Display the route between the initial start and end selections.
-  calculateAndDisplayRoute(
-    directionsRenderer,
+  displayRoute(
+    "Perth, WA",
+    "Sydney, NSW",
     directionsService,
-    markerArray,
-    stepDisplay,
-    map
-  );
-
-  // Listen to change events from the start and end lists.
-  const onChangeHandler = function () {
-    calculateAndDisplayRoute(
-      directionsRenderer,
-      directionsService,
-      markerArray,
-      stepDisplay,
-      map
-    );
-  };
-  (document.getElementById("start") as HTMLElement).addEventListener(
-    "change",
-    onChangeHandler
-  );
-  (document.getElementById("end") as HTMLElement).addEventListener(
-    "change",
-    onChangeHandler
+    directionsRenderer
   );
 }
 
-function calculateAndDisplayRoute(
-  directionsRenderer: google.maps.DirectionsRenderer,
-  directionsService: google.maps.DirectionsService,
-  markerArray: google.maps.Marker[],
-  stepDisplay: google.maps.InfoWindow,
-  map: google.maps.Map
+function displayRoute(
+  origin: string,
+  destination: string,
+  service: google.maps.DirectionsService,
+  display: google.maps.DirectionsRenderer
 ) {
-  // First, remove any existing markers from the map.
-  for (let i = 0; i < markerArray.length; i++) {
-    markerArray[i].setMap(null);
-  }
-
-  // Retrieve the start and end locations and create a DirectionsRequest using
-  // WALKING directions.
-  directionsService.route(
+  service.route(
     {
-      origin: (document.getElementById("start") as HTMLInputElement).value,
-      destination: (document.getElementById("end") as HTMLInputElement).value,
-      travelMode: google.maps.TravelMode.WALKING,
+      origin: origin,
+      destination: destination,
+      waypoints: [
+        { location: "Adelaide, SA" },
+        { location: "Broken Hill, NSW" },
+      ],
+      travelMode: google.maps.TravelMode.DRIVING,
+      avoidTolls: true,
     },
     (
       result: google.maps.DirectionsResult | null,
       status: google.maps.DirectionsStatus
     ) => {
-      // Route the directions and pass the response to a function to create
-      // markers for each step.
       if (status === "OK" && result) {
-        (document.getElementById("warnings-panel") as HTMLElement).innerHTML =
-          "<b>" + result.routes[0].warnings + "</b>";
-        directionsRenderer.setDirections(result);
-        showSteps(result, markerArray, stepDisplay, map);
+        display.setDirections(result);
       } else {
-        window.alert("Directions request failed due to " + status);
+        alert("Could not display directions due to: " + status);
       }
     }
   );
 }
 
-function showSteps(
-  directionResult: google.maps.DirectionsResult | null,
-  markerArray: google.maps.Marker[],
-  stepDisplay: google.maps.InfoWindow,
-  map: google.maps.Map
-) {
-  // For each step, place a marker, and add the text to the marker's infowindow.
-  // Also attach the marker to an array so we can keep track of it and remove it
-  // when calculating new routes.
-  const myRoute = directionResult!.routes[0]!.legs[0]!;
+function computeTotalDistance(result: google.maps.DirectionsResult) {
+  let total = 0;
+  const myroute = result.routes[0];
 
-  for (let i = 0; i < myRoute.steps.length; i++) {
-    const marker = (markerArray[i] =
-      markerArray[i] || new google.maps.Marker());
-    marker.setMap(map);
-    marker.setPosition(myRoute.steps[i].start_location);
-    attachInstructionText(
-      stepDisplay,
-      marker,
-      myRoute.steps[i].instructions,
-      map
-    );
+  if (!myroute) {
+    return;
   }
-}
 
-function attachInstructionText(
-  stepDisplay: google.maps.InfoWindow,
-  marker: google.maps.Marker,
-  text: string,
-  map: google.maps.Map
-) {
-  google.maps.event.addListener(marker, "click", () => {
-    // Open an info window when the marker is clicked on, containing the text
-    // of the step.
-    stepDisplay.setContent(text);
-    stepDisplay.open(map, marker);
-  });
+  for (let i = 0; i < myroute.legs.length; i++) {
+    total += myroute.legs[i]!.distance!.value;
+  }
+  total = total / 1000;
+  (document.getElementById("total") as HTMLElement).innerHTML = total + " km";
 }
 export { initMap };
