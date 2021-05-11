66a465dfe2572af2b9ecf4b0bf016774f3ce92ac
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:44:08 +0000

    chore: sync marker-simple [skip-ci]

--- src/index.ts
+++ src/index.ts
@@ -16,59 +16,21 @@
 /* eslint-disable no-undef, @typescript-eslint/no-unused-vars, no-unused-vars */
 import "./style.css";
 
-// In the following example, markers appear when the user clicks on the map.
-// The markers are stored in an array.
-// The user can then click an option to hide, show or delete the markers.
-let map: google.maps.Map;
-let markers: google.maps.Marker[] = [];
-
 function initMap(): void {
-  const haightAshbury = { lat: 37.769, lng: -122.446 };
+  const myLatLng = { lat: -25.363, lng: 131.044 };
 
-  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-    zoom: 12,
-    center: haightAshbury,
-    mapTypeId: "terrain",
+  const map = new google.maps.Map(
+    document.getElementById("map") as HTMLElement,
+    {
+      zoom: 4,
+      center: myLatLng,
+    }
+  );
+
+  new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "Hello World!",
   });
-
-  // This event listener will call addMarker() when the map is clicked.
-  map.addListener("click", (event) => {
-    addMarker(event.latLng);
-  });
-
-  // Adds a marker at the center of the map.
-  addMarker(haightAshbury);
 }
-
-// Adds a marker to the map and push to the array.
-function addMarker(location) {
-  const marker = new google.maps.Marker({
-    position: location,
-    map: map,
-  });
-  markers.push(marker);
-}
-
-// Sets the map on all markers in the array.
-function setMapOnAll(map: google.maps.Map | null) {
-  for (let i = 0; i < markers.length; i++) {
-    markers[i].setMap(map);
-  }
-}
-
-// Removes the markers from the map, but keeps them in the array.
-function clearMarkers() {
-  setMapOnAll(null);
-}
-
-// Shows any markers currently in the array.
-function showMarkers() {
-  setMapOnAll(map);
-}
-
-// Deletes all markers in the array by removing references to them.
-function deleteMarkers() {
-  clearMarkers();
-  markers = [];
-}
-export { initMap, clearMarkers, showMarkers, deleteMarkers };
+export { initMap };
