40f326897f77ccc157edac72668c1be684d9bdfe
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:46:19 +0000

    chore: sync places-placeid-geocoder [skip-ci]

--- src/index.ts
+++ src/index.ts
@@ -16,14 +16,12 @@
 /* eslint-disable no-undef, @typescript-eslint/no-unused-vars, no-unused-vars */
 import "./style.css";
 
-// This sample uses the Place Autocomplete widget to allow the user to search
-// for and select a place. The sample then displays an info window containing
-// the place ID and other information about the place that the user has
-// selected.
+// @ts-nocheck TODO(jpoehnelt) remove when fixed
 
-// This example requires the Places library. Include the libraries=places
+// This sample requires the Places library. Include the libraries=places
 // parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+// <script
+// src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
 function initMap(): void {
   const map = new google.maps.Map(
@@ -37,10 +35,11 @@ function initMap(): void {
   const input = document.getElementById("pac-input") as HTMLInputElement;
 
   const autocomplete = new google.maps.places.Autocomplete(input);
+
   autocomplete.bindTo("bounds", map);
 
   // Specify just the place data fields that you need.
-  autocomplete.setFields(["place_id", "geometry", "name"]);
+  autocomplete.setFields(["place_id", "geometry", "name", "formatted_address"]);
 
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
@@ -50,47 +49,45 @@ function initMap(): void {
   ) as HTMLElement;
   infowindow.setContent(infowindowContent);
 
-  const marker = new google.maps.Marker({ map: map });
+  const geocoder = new google.maps.Geocoder();
 
+  const marker = new google.maps.Marker({ map: map });
   marker.addListener("click", () => {
     infowindow.open(map, marker);
   });
 
   autocomplete.addListener("place_changed", () => {
     infowindow.close();
-
     const place = autocomplete.getPlace();
 
-    if (!place.geometry || !place.geometry.location) {
+    if (!place.place_id) {
       return;
     }
+    geocoder.geocode({ placeId: place.place_id }, (results, status) => {
+      if (status !== "OK" && results) {
+        window.alert("Geocoder failed due to: " + status);
+        return;
+      }
 
-    if (place.geometry.viewport) {
-      map.fitBounds(place.geometry.viewport);
-    } else {
-      map.setCenter(place.geometry.location);
-      map.setZoom(17);
-    }
+      map.setZoom(11);
+      map.setCenter(results[0].geometry.location);
 
-    // Set the position of the marker using the place ID and location.
-    // @ts-ignore This should be in @typings/googlemaps.
-    marker.setPlace({
-      placeId: place.place_id,
-      location: place.geometry.location,
+      // Set the position of the marker using the place ID and location.
+      // @ts-ignore TODO(jpoehnelt) This should be in @typings/googlemaps.
+      marker.setPlace({
+        placeId: place.place_id,
+        location: results[0].geometry.location,
+      });
+
+      marker.setVisible(true);
+
+      infowindowContent.children["place-name"].textContent = place.name;
+      infowindowContent.children["place-id"].textContent = place.place_id;
+      infowindowContent.children["place-address"].textContent =
+        results[0].formatted_address;
+
+      infowindow.open(map, marker);
     });
-
-    marker.setVisible(true);
-
-    (
-      infowindowContent.children.namedItem("place-name") as HTMLElement
-    ).textContent = place.name as string;
-    (
-      infowindowContent.children.namedItem("place-id") as HTMLElement
-    ).textContent = place.place_id as string;
-    (
-      infowindowContent.children.namedItem("place-address") as HTMLElement
-    ).textContent = place.formatted_address as string;
-    infowindow.open(map, marker);
   });
 }
 export { initMap };
