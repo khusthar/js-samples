ed6be8947712909fe2ba3953ad0c1a1b2bb0829e
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:36:55 +0000

    chore: sync elevation-paths [skip-ci]

--- src/index.ts
+++ src/index.ts
@@ -16,34 +16,95 @@
 /* eslint-disable no-undef, @typescript-eslint/no-unused-vars, no-unused-vars */
 import "./style.css";
 
-let map: google.maps.Map;
+// Load the Visualization API and the columnchart package.
+// @ts-ignore TODO(jpoehnelt) update to newest visualization library
+google.load("visualization", "1", { packages: ["columnchart"] });
 
 function initMap(): void {
-  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-    zoom: 2,
-    center: new google.maps.LatLng(2.8, -187.3),
-    mapTypeId: "terrain",
-  });
+  // The following path marks a path from Mt. Whitney, the highest point in the
+  // continental United States to Badwater, Death Valley, the lowest point.
+  const path = [
+    { lat: 36.579, lng: -118.292 }, // Mt. Whitney
+    { lat: 36.606, lng: -118.0638 }, // Lone Pine
+    { lat: 36.433, lng: -117.951 }, // Owens Lake
+    { lat: 36.588, lng: -116.943 }, // Beatty Junction
+    { lat: 36.34, lng: -117.468 }, // Panama Mint Springs
+    { lat: 36.24, lng: -116.832 },
+  ]; // Badwater, Death Valley
 
-  // Create a <script> tag and set the USGS URL as the source.
-  const script = document.createElement("script");
-  // This example uses a local copy of the GeoJSON stored at
-  // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-  script.src =
-    "https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js";
-  document.getElementsByTagName("head")[0].appendChild(script);
+  const map = new google.maps.Map(
+    document.getElementById("map") as HTMLElement,
+    {
+      zoom: 8,
+      center: path[1],
+      mapTypeId: "terrain",
+    }
+  );
+
+  // Create an ElevationService.
+  const elevator = new google.maps.ElevationService();
+
+  // Draw the path, using the Visualization API and the Elevation service.
+  displayPathElevation(path, elevator, map);
 }
 
-// Loop through the results array and place a marker for each
-// set of coordinates.
-const eqfeed_callback = function (results: any) {
-  for (let i = 0; i < results.features.length; i++) {
-    const coords = results.features[i].geometry.coordinates;
-    const latLng = new google.maps.LatLng(coords[1], coords[0]);
-    new google.maps.Marker({
-      position: latLng,
-      map: map,
-    });
+function displayPathElevation(
+  path: google.maps.LatLngLiteral[],
+  elevator: google.maps.ElevationService,
+  map: google.maps.Map
+) {
+  // Display a polyline of the elevation path.
+  new google.maps.Polyline({
+    path: path,
+    strokeColor: "#0000CC",
+    strokeOpacity: 0.4,
+    map: map,
+  });
+
+  // Create a PathElevationRequest object using this array.
+  // Ask for 256 samples along that path.
+  // Initiate the path request.
+  elevator.getElevationAlongPath(
+    {
+      path: path,
+      samples: 256,
+    },
+    plotElevation
+  );
+}
+
+// Takes an array of ElevationResult objects, draws the path on the map
+// and plots the elevation profile on a Visualization API ColumnChart.
+function plotElevation(elevations, status) {
+  const chartDiv = document.getElementById("elevation_chart") as HTMLElement;
+
+  if (status !== "OK") {
+    // Show the error code inside the chartDiv.
+    chartDiv.innerHTML =
+      "Cannot show elevation: request failed because " + status;
+    return;
   }
-};
-export { initMap, eqfeed_callback };
+  // Create a new chart in the elevation_chart DIV.
+  const chart = new google.visualization.ColumnChart(chartDiv);
+
+  // Extract the data from which to populate the chart.
+  // Because the samples are equidistant, the 'Sample'
+  // column here does double duty as distance along the
+  // X axis.
+  const data = new google.visualization.DataTable();
+  data.addColumn("string", "Sample");
+  data.addColumn("number", "Elevation");
+
+  for (let i = 0; i < elevations.length; i++) {
+    data.addRow(["", elevations[i].elevation]);
+  }
+
+  // Draw the chart using the data within its DIV.
+  chart.draw(data, {
+    height: 150,
+    legend: "none",
+    // @ts-ignore TODO(jpoehnelt) update to newest visualization library
+    titleY: "Elevation (m)",
+  });
+}
+export { initMap };
