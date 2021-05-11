49c22891fa7f9f968d7ebfc6a5ee0f327e7dbcc0
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:44:00 +0000

    chore: sync marker-remove [skip-ci]

--- src/index.ts
+++ src/index.ts
@@ -17,37 +17,58 @@
 import "./style.css";
 
 // In the following example, markers appear when the user clicks on the map.
-// Each marker is labeled with a single alphabetical character.
-const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let labelIndex = 0;
+// The markers are stored in an array.
+// The user can then click an option to hide, show or delete the markers.
+let map: google.maps.Map;
+let markers: google.maps.Marker[] = [];
 
 function initMap(): void {
-  const bangalore = { lat: 12.97, lng: 77.59 };
-  const map = new google.maps.Map(
-    document.getElementById("map") as HTMLElement,
-    {
-      zoom: 12,
-      center: bangalore,
-    }
-  );
+  const haightAshbury = { lat: 37.769, lng: -122.446 };
 
-  // This event listener calls addMarker() when the map is clicked.
-  google.maps.event.addListener(map, "click", (event) => {
-    addMarker(event.latLng, map);
+  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+    zoom: 12,
+    center: haightAshbury,
+    mapTypeId: "terrain",
   });
 
-  // Add a marker at the center of the map.
-  addMarker(bangalore, map);
+  // This event listener will call addMarker() when the map is clicked.
+  map.addListener("click", (event) => {
+    addMarker(event.latLng);
+  });
+
+  // Adds a marker at the center of the map.
+  addMarker(haightAshbury);
 }
 
-// Adds a marker to the map.
-function addMarker(location: google.maps.LatLngLiteral, map: google.maps.Map) {
-  // Add the marker at the clicked location, and add the next-available label
-  // from the array of alphabetical characters.
-  new google.maps.Marker({
+// Adds a marker to the map and push to the array.
+function addMarker(location) {
+  const marker = new google.maps.Marker({
     position: location,
-    label: labels[labelIndex++ % labels.length],
     map: map,
   });
+  markers.push(marker);
 }
-export { initMap };
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map: google.maps.Map | null) {
+  for (let i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+  setMapOnAll(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+  setMapOnAll(map);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+  clearMarkers();
+  markers = [];
+}
+export { initMap, clearMarkers, showMarkers, deleteMarkers };
