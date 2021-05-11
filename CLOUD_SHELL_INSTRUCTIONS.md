8e2779bb3118307b344eb5d4457c54b688637e96
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:35:49 +0000

    chore: sync disable-zoom-and-pan [skip-ci]

--- CLOUD_SHELL_INSTRUCTIONS.md
+++ CLOUD_SHELL_INSTRUCTIONS.md
@@ -1,11 +1,11 @@
-# Run the `directions-waypoints` sample in Google Cloud Shell
+# Run the `disable-zoom-and-pan` sample in Google Cloud Shell
 
 <walkthrough-tutorial-duration duration="10"/>
 
 ## Introduction
 
 This tutorial will walk through the process of running a web application from
-the `directions-waypoints` sample using TypeScript, Babel, and Webpack.
+the `disable-zoom-and-pan` sample using TypeScript, Babel, and Webpack.
 
 For feedback related to this sample, please open a new issue on
 [GitHub](https://github.com/googlemaps/js-samples/issues).
--- src/index.ts
+++ src/index.ts
@@ -17,73 +17,20 @@
 import "./style.css";
 
 function initMap(): void {
-  const directionsService = new google.maps.DirectionsService();
-  const directionsRenderer = new google.maps.DirectionsRenderer();
+  const locationRio = { lat: -22.915, lng: -43.197 };
   const map = new google.maps.Map(
     document.getElementById("map") as HTMLElement,
     {
-      zoom: 6,
-      center: { lat: 41.85, lng: -87.65 },
+      zoom: 13,
+      center: locationRio,
+      gestureHandling: "none",
+      zoomControl: false,
     }
   );
-  directionsRenderer.setMap(map);
-
-  (document.getElementById("submit") as HTMLElement).addEventListener(
-    "click",
-    () => {
-      calculateAndDisplayRoute(directionsService, directionsRenderer);
-    }
-  );
-}
-
-function calculateAndDisplayRoute(
-  directionsService: google.maps.DirectionsService,
-  directionsRenderer: google.maps.DirectionsRenderer
-) {
-  const waypts: google.maps.DirectionsWaypoint[] = [];
-  const checkboxArray = document.getElementById(
-    "waypoints"
-  ) as HTMLSelectElement;
-
-  for (let i = 0; i < checkboxArray.length; i++) {
-    if (checkboxArray.options[i].selected) {
-      waypts.push({
-        location: (checkboxArray[i] as HTMLOptionElement).value,
-        stopover: true,
-      });
-    }
-  }
-
-  directionsService.route(
-    {
-      origin: (document.getElementById("start") as HTMLInputElement).value,
-      destination: (document.getElementById("end") as HTMLInputElement).value,
-      waypoints: waypts,
-      optimizeWaypoints: true,
-      travelMode: google.maps.TravelMode.DRIVING,
-    },
-    (response, status) => {
-      if (status === "OK" && response) {
-        directionsRenderer.setDirections(response);
-        const route = response.routes[0];
-        const summaryPanel = document.getElementById(
-          "directions-panel"
-        ) as HTMLElement;
-        summaryPanel.innerHTML = "";
-
-        // For each route, display summary information.
-        for (let i = 0; i < route.legs.length; i++) {
-          const routeSegment = i + 1;
-          summaryPanel.innerHTML +=
-            "<b>Route Segment: " + routeSegment + "</b><br>";
-          summaryPanel.innerHTML += route.legs[i].start_address + " to ";
-          summaryPanel.innerHTML += route.legs[i].end_address + "<br>";
-          summaryPanel.innerHTML += route.legs[i].distance!.text + "<br><br>";
-        }
-      } else {
-        window.alert("Directions request failed due to " + status);
-      }
-    }
-  );
+  new google.maps.Marker({
+    position: locationRio,
+    map,
+    title: "Hello World!",
+  });
 }
 export { initMap };
