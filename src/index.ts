bb2b142a733143e93b978a0db74bf4dc6e79b11e
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:36:50 +0000

    chore: sync earthquake-markers [skip-ci]

--- src/index.ts
+++ src/index.ts
@@ -22,12 +22,11 @@ function initMap(): void {
   map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
     zoom: 2,
     center: new google.maps.LatLng(2.8, -187.3),
-    mapTypeId: google.maps.MapTypeId.TERRAIN,
+    mapTypeId: "terrain",
   });
 
   // Create a <script> tag and set the USGS URL as the source.
   const script = document.createElement("script");
-
   // This example uses a local copy of the GeoJSON stored at
   // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
   script.src =
@@ -35,24 +34,16 @@ function initMap(): void {
   document.getElementsByTagName("head")[0].appendChild(script);
 }
 
+// Loop through the results array and place a marker for each
+// set of coordinates.
 const eqfeed_callback = function (results: any) {
-  // TODO fix @types/googlemaps
-  const heatmapData: any[] = [];
-
   for (let i = 0; i < results.features.length; i++) {
     const coords = results.features[i].geometry.coordinates;
     const latLng = new google.maps.LatLng(coords[1], coords[0]);
-    const magnitude = results.features[i].properties.mag;
-    const weightedLoc = {
-      location: latLng,
-      weight: Math.pow(2, magnitude),
-    };
-    heatmapData.push(weightedLoc);
+    new google.maps.Marker({
+      position: latLng,
+      map: map,
+    });
   }
-  const heatmap = new google.maps.visualization.HeatmapLayer({
-    data: heatmapData,
-    dissipating: false,
-    map: map,
-  });
 };
 export { initMap, eqfeed_callback };
