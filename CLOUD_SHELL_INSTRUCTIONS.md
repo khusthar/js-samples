2033e630030025d5cb8ea4a9ca0dbcdeb2f5eceb
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:42:58 +0000

    chore: sync maptype-base [skip-ci]

--- CLOUD_SHELL_INSTRUCTIONS.md
+++ CLOUD_SHELL_INSTRUCTIONS.md
@@ -1,11 +1,11 @@
-# Run the `map-sync` sample in Google Cloud Shell
+# Run the `maptype-base` sample in Google Cloud Shell
 
 <walkthrough-tutorial-duration duration="10"/>
 
 ## Introduction
 
 This tutorial will walk through the process of running a web application from
-the `map-sync` sample using TypeScript, Babel, and Webpack.
+the `maptype-base` sample using TypeScript, Babel, and Webpack.
 
 For feedback related to this sample, please open a new issue on
 [GitHub](https://github.com/googlemaps/js-samples/issues).
--- src/index.ts
+++ src/index.ts
@@ -16,9 +16,71 @@
 /* eslint-disable no-undef, @typescript-eslint/no-unused-vars, no-unused-vars */
 import "./style.css";
 
-const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-  center: { lat: -34.397, lng: 150.644 },
-  zoom: 8,
-});
+/*
+ * This demo demonstrates how to replace default map tiles with custom imagery.
+ * In this case, the CoordMapType displays gray tiles annotated with the tile
+ * coordinates.
+ *
+ * Try panning and zooming the map to see how the coordinates change.
+ */
 
-export { map };
+class CoordMapType {
+  tileSize: google.maps.Size;
+  maxZoom = 19;
+  name = "Tile #s";
+  alt = "Tile Coordinate Map Type";
+
+  constructor(tileSize: google.maps.Size) {
+    this.tileSize = tileSize;
+  }
+
+  getTile(
+    coord: google.maps.Point,
+    zoom: number,
+    ownerDocument: Document
+  ): HTMLElement {
+    const div = ownerDocument.createElement("div");
+    div.innerHTML = String(coord);
+    div.style.width = this.tileSize.width + "px";
+    div.style.height = this.tileSize.height + "px";
+    div.style.fontSize = "10";
+    div.style.borderStyle = "solid";
+    div.style.borderWidth = "1px";
+    div.style.borderColor = "#AAAAAA";
+    div.style.backgroundColor = "#E5E3DF";
+    return div;
+  }
+
+  releaseTile(tile: HTMLElement): void {}
+}
+
+function initMap(): void {
+  const map = new google.maps.Map(
+    document.getElementById("map") as HTMLElement,
+    {
+      zoom: 10,
+      center: { lat: 41.85, lng: -87.65 },
+      streetViewControl: false,
+      mapTypeId: "coordinate",
+      mapTypeControlOptions: {
+        mapTypeIds: ["coordinate", "roadmap"],
+        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+      },
+    }
+  );
+
+  map.addListener("maptypeid_changed", () => {
+    const showStreetViewControl =
+      (map.getMapTypeId() as string) !== "coordinate";
+    map.setOptions({
+      streetViewControl: showStreetViewControl,
+    });
+  });
+
+  // Now attach the coordinate map type to the map's registry.
+  map.mapTypes.set(
+    "coordinate",
+    new CoordMapType(new google.maps.Size(256, 256))
+  );
+}
+export { initMap };
