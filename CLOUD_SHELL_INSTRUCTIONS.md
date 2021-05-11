ab44442c3a992c11ae2f0b19c8bc70f9779714f2
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:37:18 +0000

    chore: sync event-closure [skip-ci]

--- CLOUD_SHELL_INSTRUCTIONS.md
+++ CLOUD_SHELL_INSTRUCTIONS.md
@@ -1,11 +1,11 @@
-# Run the `event-click-latlng` sample in Google Cloud Shell
+# Run the `event-closure` sample in Google Cloud Shell
 
 <walkthrough-tutorial-duration duration="10"/>
 
 ## Introduction
 
 This tutorial will walk through the process of running a web application from
-the `event-click-latlng` sample using TypeScript, Babel, and Webpack.
+the `event-closure` sample using TypeScript, Babel, and Webpack.
 
 For feedback related to this sample, please open a new issue on
 [GitHub](https://github.com/googlemaps/js-samples/issues).
--- src/index.ts
+++ src/index.ts
@@ -16,34 +16,57 @@
 /* eslint-disable no-undef, @typescript-eslint/no-unused-vars, no-unused-vars */
 import "./style.css";
 
-function initMap() {
-  const myLatlng = { lat: -25.363, lng: 131.044 };
+function initMap(): void {
+  const map = new google.maps.Map(
+    document.getElementById("map") as HTMLElement,
+    {
+      zoom: 4,
+      center: { lat: -25.363882, lng: 131.044922 },
+    }
+  );
 
-  const map = new google.maps.Map(document.getElementById("map")!, {
-    zoom: 4,
-    center: myLatlng,
-  });
+  const bounds: google.maps.LatLngBoundsLiteral = {
+    north: -25.363882,
+    south: -31.203405,
+    east: 131.044922,
+    west: 125.244141,
+  };
 
-  // Create the initial InfoWindow.
-  let infoWindow = new google.maps.InfoWindow({
-    content: "Click the map to get Lat/Lng!",
-    position: myLatlng,
-  });
-  infoWindow.open(map);
+  // Display the area between the location southWest and northEast.
+  map.fitBounds(bounds);
 
-  // Configure the click listener.
-  map.addListener("click", (mapsMouseEvent) => {
-    // Close the current InfoWindow.
-    infoWindow.close();
+  // Add 5 markers to map at random locations.
+  // For each of these markers, give them a title with their index, and when
+  // they are clicked they should open an infowindow with text from a secret
+  // message.
+  const secretMessages = ["This", "is", "the", "secret", "message"];
+  const lngSpan = bounds.east - bounds.west;
+  const latSpan = bounds.north - bounds.south;
 
-    // Create a new InfoWindow.
-    infoWindow = new google.maps.InfoWindow({
-      position: mapsMouseEvent.latLng,
+  for (let i = 0; i < secretMessages.length; ++i) {
+    const marker = new google.maps.Marker({
+      position: {
+        lat: bounds.south + latSpan * Math.random(),
+        lng: bounds.west + lngSpan * Math.random(),
+      },
+      map: map,
     });
-    infoWindow.setContent(
-      JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
-    );
-    infoWindow.open(map);
+    attachSecretMessage(marker, secretMessages[i]);
+  }
+}
+
+// Attaches an info window to a marker with the provided message. When the
+// marker is clicked, the info window will open with the secret message.
+function attachSecretMessage(
+  marker: google.maps.Marker,
+  secretMessage: string
+) {
+  const infowindow = new google.maps.InfoWindow({
+    content: secretMessage,
+  });
+
+  marker.addListener("click", () => {
+    infowindow.open(marker.get("map"), marker);
   });
 }
 export { initMap };
