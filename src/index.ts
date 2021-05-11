9cfab5a74add1449d56e646ee1c8ebc9cd81a92d
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:48:13 +0000

    chore: sync streetview-custom-tiles [skip-ci]

--- src/index.ts
+++ src/index.ts
@@ -18,60 +18,98 @@ import "./style.css";
 
 // @ts-nocheck TODO(jpoehnelt) remove when fixed
 
-function initPano() {
-  // Set up Street View and initially set it visible. Register the
-  // custom panorama provider function. Set the StreetView to display
-  // the custom panorama 'reception' which we check for below.
-  const panorama = new google.maps.StreetViewPanorama(
-    document.getElementById("map") as HTMLElement,
-    { pano: "reception", visible: true }
-  );
-  panorama.registerPanoProvider(getCustomPanorama);
+let panorama: google.maps.StreetViewPanorama;
+
+// StreetViewPanoramaData of a panorama just outside the Google Sydney office.
+let outsideGoogle: google.maps.StreetViewPanoramaData;
+
+// StreetViewPanoramaData for a custom panorama: the Google Sydney reception.
+function getReceptionPanoramaData(): google.maps.StreetViewPanoramaData {
+  return {
+    location: {
+      pano: "reception", // The ID for this custom panorama.
+      description: "Google Sydney - Reception",
+      latLng: new google.maps.LatLng(-33.86684, 151.19583),
+    },
+    links: [
+      {
+        heading: 195,
+        description: "Exit",
+        pano: (outsideGoogle.location as google.maps.StreetViewLocation).pano,
+      },
+    ],
+    copyright: "Imagery (c) 2010 Google",
+    tiles: {
+      tileSize: new google.maps.Size(1024, 512),
+      worldSize: new google.maps.Size(2048, 1024),
+      centerHeading: 105,
+      getTileUrl: function (
+        pano: string,
+        zoom: number,
+        tileX: number,
+        tileY: number
+      ): string {
+        return (
+          "https://developers.google.com/maps/documentation/javascript/examples/full/images/" +
+          "panoReception1024-" +
+          zoom +
+          "-" +
+          tileX +
+          "-" +
+          tileY +
+          ".jpg"
+        );
+      },
+    },
+  };
 }
 
-// Return a pano image given the panoID.
-function getCustomPanoramaTileUrl(
-  pano: string,
-  zoom: number,
-  tileX: number,
-  tileY: number
-): string {
-  return (
-    "https://developers.google.com/maps/documentation/javascript/examples/full/images/" +
-    "panoReception1024-" +
-    zoom +
-    "-" +
-    tileX +
-    "-" +
-    tileY +
-    ".jpg"
+function initPanorama() {
+  panorama = new google.maps.StreetViewPanorama(
+    document.getElementById("street-view") as HTMLElement,
+    { pano: (outsideGoogle.location as google.maps.StreetViewLocation).pano }
   );
-}
+  // Register a provider for the custom panorama.
+  panorama.registerPanoProvider(
+    (pano: string): google.maps.StreetViewPanoramaData => {
+      if (pano === "reception") {
+        return getReceptionPanoramaData();
+      }
+      // @ts-ignore TODO(jpoehnelt) fix typings
+      return null;
+    }
+  );
 
-// Construct the appropriate StreetViewPanoramaData given
-// the passed pano IDs.
-function getCustomPanorama(pano: string): google.maps.StreetViewPanoramaData {
-  if (pano === "reception") {
-    return {
-      location: {
+  // Add a link to our custom panorama from outside the Google Sydney office.
+  panorama.addListener("links_changed", () => {
+    if (
+      panorama.getPano() ===
+      (outsideGoogle.location as google.maps.StreetViewLocation).pano
+    ) {
+      panorama.getLinks().push({
+        description: "Google Sydney",
+        heading: 25,
         pano: "reception",
-        description: "Google Sydney - Reception",
-      },
-      links: [],
-      // The text for the copyright control.
-      copyright: "Imagery (c) 2010 Google",
-      // The definition of the tiles for this panorama.
-      tiles: {
-        tileSize: new google.maps.Size(1024, 512),
-        worldSize: new google.maps.Size(2048, 1024),
-        // The heading in degrees at the origin of the panorama
-        // tile set.
-        centerHeading: 105,
-        getTileUrl: getCustomPanoramaTileUrl,
-      },
-    };
-  }
-  // @ts-ignore TODO(jpoehnelt) fix typings
-  return null;
+      });
+    }
+  });
 }
-export { initPano };
+
+function initMap(): void {
+  // Use the Street View service to find a pano ID on Pirrama Rd, outside the
+  // Google office.
+  const streetviewService = new google.maps.StreetViewService();
+  streetviewService.getPanorama(
+    { location: { lat: -33.867386, lng: 151.195767 } },
+    (
+      result: google.maps.StreetViewPanoramaData | null,
+      status: google.maps.StreetViewStatus
+    ) => {
+      if (status === google.maps.StreetViewStatus.OK && result) {
+        outsideGoogle = result;
+        initPanorama();
+      }
+    }
+  );
+}
+export { initMap };
