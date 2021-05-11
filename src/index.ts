dd5a04f85de1d79b49edc5a1dd7f787fcb2f6684
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:39:17 +0000

    chore: sync interaction-disable-pan-zoom [skip-ci]

--- src/index.ts
+++ src/index.ts
@@ -16,11 +16,6 @@
 /* eslint-disable no-undef, @typescript-eslint/no-unused-vars, no-unused-vars */
 import "./style.css";
 
-/**
- * This sample sets the gesture handling mode to 'cooperative',
- * which means that on a mobile device, the user must swipe with one
- * finger to scroll the page and two fingers to pan the map.
- */
 function initMap(): void {
   const center = { lat: -25.363, lng: 131.044 };
   const zoom = 4;
@@ -28,6 +23,8 @@ function initMap(): void {
   new google.maps.Map(document.getElementById("map")!, {
     zoom,
     center,
+    gestureHandling: "none",
+    zoomControl: false,
   });
 }
 export { initMap };
