235ae41ab69f352af2b902780eab25a2a9a73276
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:42:21 +0000

    chore: sync map-language [skip-ci]

--- src/index.ts
+++ src/index.ts
@@ -16,14 +16,18 @@
 /* eslint-disable no-undef, @typescript-eslint/no-unused-vars, no-unused-vars */
 import "./style.css";
 
+// This example displays a map with the language and region set
+// to Japan. These settings are specified in the HTML script element
+// when loading the Google Maps JavaScript API.
+// Setting the language shows the map in the language of your choice.
+// Setting the region biases the geocoding results to that region.
 function initMap(): void {
-  new google.maps.Map(
+  const map = new google.maps.Map(
     document.getElementById("map") as HTMLElement,
     {
-      mapId: "8e0a97af9386fef",
-      center: { lat: 48.85, lng: 2.35 },
-      zoom: 12,
-    } as google.maps.MapOptions
+      zoom: 8,
+      center: { lat: 35.717, lng: 139.731 },
+    }
   );
 }
 export { initMap };
