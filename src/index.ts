0fcd60d8276a2c8e8c3ddd39ebd5ef4b89ea42a4
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:43:20 +0000

    chore: sync maptype-styled-simple [skip-ci]

--- src/index.ts
+++ src/index.ts
@@ -16,57 +16,138 @@
 /* eslint-disable no-undef, @typescript-eslint/no-unused-vars, no-unused-vars */
 import "./style.css";
 
-/*
- * This demo illustrates the coordinate system used to display map tiles in the
- * API.
- *
- * Tiles in Google Maps are numbered from the same origin as that for
- * pixels. For Google's implementation of the Mercator projection, the origin
- * tile is always at the northwest corner of the map, with x values increasing
- * from west to east and y values increasing from north to south.
- *
- * Try panning and zooming the map to see how the coordinates change.
- */
-
-class CoordMapType {
-  tileSize: google.maps.Size;
-
-  constructor(tileSize: google.maps.Size) {
-    this.tileSize = tileSize;
-  }
-  getTile(
-    coord: google.maps.Point,
-    zoom: number,
-    ownerDocument: Document
-  ): HTMLElement {
-    const div = ownerDocument.createElement("div");
-    div.innerHTML = String(coord);
-    div.style.width = this.tileSize.width + "px";
-    div.style.height = this.tileSize.height + "px";
-    div.style.fontSize = "10";
-    div.style.borderStyle = "solid";
-    div.style.borderWidth = "1px";
-    div.style.borderColor = "#AAAAAA";
-    return div;
-  }
-  releaseTile(tile: Element): void {}
-}
-
 function initMap(): void {
+  // Create a new StyledMapType object, passing it an array of styles,
+  // and the name to be displayed on the map type control.
+  const styledMapType = new google.maps.StyledMapType(
+    [
+      { elementType: "geometry", stylers: [{ color: "#ebe3cd" }] },
+      { elementType: "labels.text.fill", stylers: [{ color: "#523735" }] },
+      { elementType: "labels.text.stroke", stylers: [{ color: "#f5f1e6" }] },
+      {
+        featureType: "administrative",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#c9b2a6" }],
+      },
+      {
+        featureType: "administrative.land_parcel",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#dcd2be" }],
+      },
+      {
+        featureType: "administrative.land_parcel",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#ae9e90" }],
+      },
+      {
+        featureType: "landscape.natural",
+        elementType: "geometry",
+        stylers: [{ color: "#dfd2ae" }],
+      },
+      {
+        featureType: "poi",
+        elementType: "geometry",
+        stylers: [{ color: "#dfd2ae" }],
+      },
+      {
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#93817c" }],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "geometry.fill",
+        stylers: [{ color: "#a5b076" }],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#447530" }],
+      },
+      {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [{ color: "#f5f1e6" }],
+      },
+      {
+        featureType: "road.arterial",
+        elementType: "geometry",
+        stylers: [{ color: "#fdfcf8" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [{ color: "#f8c967" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#e9bc62" }],
+      },
+      {
+        featureType: "road.highway.controlled_access",
+        elementType: "geometry",
+        stylers: [{ color: "#e98d58" }],
+      },
+      {
+        featureType: "road.highway.controlled_access",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#db8555" }],
+      },
+      {
+        featureType: "road.local",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#806b63" }],
+      },
+      {
+        featureType: "transit.line",
+        elementType: "geometry",
+        stylers: [{ color: "#dfd2ae" }],
+      },
+      {
+        featureType: "transit.line",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#8f7d77" }],
+      },
+      {
+        featureType: "transit.line",
+        elementType: "labels.text.stroke",
+        stylers: [{ color: "#ebe3cd" }],
+      },
+      {
+        featureType: "transit.station",
+        elementType: "geometry",
+        stylers: [{ color: "#dfd2ae" }],
+      },
+      {
+        featureType: "water",
+        elementType: "geometry.fill",
+        stylers: [{ color: "#b9d3c2" }],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#92998d" }],
+      },
+    ],
+    { name: "Styled Map" }
+  );
+
+  // Create a map object, and include the MapTypeId to add
+  // to the map type control.
   const map = new google.maps.Map(
     document.getElementById("map") as HTMLElement,
     {
-      zoom: 10,
-      center: { lat: 41.85, lng: -87.65 },
+      center: { lat: 55.647, lng: 37.581 },
+      zoom: 11,
+      mapTypeControlOptions: {
+        mapTypeIds: ["roadmap", "satellite", "hybrid", "terrain", "styled_map"],
+      },
     }
   );
 
-  // Insert this overlay map type as the first overlay map type at
-  // position 0. Note that all overlay map types appear on top of
-  // their parent base map.
-  map.overlayMapTypes.insertAt(
-    0,
-    new CoordMapType(new google.maps.Size(256, 256))
-  );
+  //Associate the styled map with the MapTypeId and set it to display.
+  map.mapTypes.set("styled_map", styledMapType);
+  map.setMapTypeId("styled_map");
 }
 export { initMap };
