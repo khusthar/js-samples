a20c205443129a5093c64ceda501ddd322f8bd8d
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:41:14 +0000

    chore: sync layer-traffic [skip-ci]

--- src/index.ts
+++ src/index.ts
@@ -20,25 +20,12 @@ function initMap(): void {
   const map = new google.maps.Map(
     document.getElementById("map") as HTMLElement,
     {
-      zoom: 12,
-      center: { lat: 37.06, lng: -95.68 },
+      zoom: 13,
+      center: { lat: 34.04924594193164, lng: -118.24104309082031 },
     }
   );
 
-  const kmlLayer = new google.maps.KmlLayer({
-    url: "https://raw.githubusercontent.com/googlearchive/kml-samples/gh-pages/kml/Placemark/placemark.kml",
-    suppressInfoWindows: true,
-    map: map,
-  });
-
-  kmlLayer.addListener("click", (kmlEvent) => {
-    const text = kmlEvent.featureData.description;
-    showInContentWindow(text);
-  });
-
-  function showInContentWindow(text: string) {
-    const sidebar = document.getElementById("sidebar") as HTMLElement;
-    sidebar.innerHTML = text;
-  }
+  const trafficLayer = new google.maps.TrafficLayer();
+  trafficLayer.setMap(map);
 }
 export { initMap };
