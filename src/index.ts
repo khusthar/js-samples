7ee30d1840347ba488db142aebca49e0550a1b08
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:42:36 +0000

    chore: sync map-puzzle [skip-ci]

--- src/index.ts
+++ src/index.ts
@@ -16,149 +16,348 @@
 /* eslint-disable no-undef, @typescript-eslint/no-unused-vars, no-unused-vars */
 import "./style.css";
 
-// This example defines an image map type using the Gall-Peters
-// projection.
-// https://en.wikipedia.org/wiki/Gall%E2%80%93Peters_projection
+interface Country {
+  bounds: number[][];
+  name: string;
+  start: string[];
+  end: string[];
+}
+
+class PuzzleDemo {
+  private map_: google.maps.Map;
+  private polys_: google.maps.Polygon[] = [];
+  private difficulty_ = "Easy";
+  private count_ = 0;
+  private pieceDiv_: HTMLElement;
+  private timeDiv_: HTMLElement;
+  private dataLoaded_ = false;
+  private NUM_PIECES_ = 10;
+  private countries_: Country[] = [];
+  private timer_ = 0;
+  private START_COLOR_ = "#3c79de";
+  private END_COLOR_ = "#037e29";
+
+  constructor(map: google.maps.Map) {
+    this.map_ = map;
+    this.pieceDiv_ = document.createElement("div");
+    this.timeDiv_ = document.createElement("div");
+    this.createMenu_();
+    this.setDifficultyStyle_();
+    this.loadData_();
+  }
+
+  private createMenu_() {
+    const menuDiv = document.createElement("div");
+    menuDiv.style.cssText =
+      "margin: 40px 10px; border-radius: 8px; height: 320px; width: 180px;" +
+      "background-color: white; font-size: 14px; font-family: Roboto;" +
+      "text-align: center; color: grey;line-height: 32px; overflow: hidden";
+    const titleDiv = document.createElement("div");
+    titleDiv.style.cssText =
+      "width: 100%; background-color: #4285f4; color: white; font-size: 20px;" +
+      "line-height: 40px;margin-bottom: 24px";
+    titleDiv.innerText = "Game Options";
+    const pieceTitleDiv = document.createElement("div");
+    pieceTitleDiv.innerText = "PIECE:";
+    pieceTitleDiv.style.fontWeight = "800";
+
+    const pieceDiv = this.pieceDiv_;
+    pieceDiv.innerText = "0 / " + this.NUM_PIECES_;
+    const timeTitleDiv = document.createElement("div");
+    timeTitleDiv.innerText = "TIME:";
+    timeTitleDiv.style.fontWeight = "800";
+
+    const timeDiv = this.timeDiv_;
+    timeDiv.innerText = "0.0 seconds";
+    const difficultyTitleDiv = document.createElement("div");
+    difficultyTitleDiv.innerText = "DIFFICULTY:";
+    difficultyTitleDiv.style.fontWeight = "800";
+    const difficultySelect = document.createElement("select");
+    ["Easy", "Moderate", "Hard", "Extreme"].forEach((level) => {
+      const option = document.createElement("option");
+      option.value = level.toLowerCase();
+      option.innerText = level;
+      difficultySelect.appendChild(option);
+    });
+    difficultySelect.style.cssText =
+      "border: 2px solid lightgrey; background-color: white; color: #4275f4;" +
+      "padding: 6px;";
+
+    difficultySelect.onchange = () => {
+      this.setDifficulty_(difficultySelect.value);
+      this.resetGame_();
+    };
+    const resetDiv = document.createElement("div");
+    resetDiv.innerText = "Reset";
+    resetDiv.style.cssText =
+      "cursor: pointer; border-top: 1px solid lightgrey; margin-top: 18px;" +
+      "color: #4275f4; line-height: 40px; font-weight: 800";
+    resetDiv.onclick = this.resetGame_.bind(this);
+    menuDiv.appendChild(titleDiv);
+    menuDiv.appendChild(pieceTitleDiv);
+    menuDiv.appendChild(pieceDiv);
+    menuDiv.appendChild(timeTitleDiv);
+    menuDiv.appendChild(timeDiv);
+    menuDiv.appendChild(difficultyTitleDiv);
+    menuDiv.appendChild(difficultySelect);
+    menuDiv.appendChild(resetDiv);
+    this.map_.controls[google.maps.ControlPosition.TOP_LEFT].push(menuDiv);
+  }
+
+  render() {
+    if (!this.dataLoaded_) {
+      return;
+    }
+    this.start_();
+  }
+
+  private loadData_() {
+    const xmlhttpRequest = new XMLHttpRequest();
+
+    xmlhttpRequest.onreadystatechange = () => {
+      if (
+        xmlhttpRequest.status != 200 ||
+        xmlhttpRequest.readyState != XMLHttpRequest.DONE
+      )
+        return;
+      this.loadDataComplete_(JSON.parse(xmlhttpRequest.responseText) as any);
+    };
+    xmlhttpRequest.open(
+      "GET",
+      "https://storage.googleapis.com/mapsdevsite/json/puzzle.json",
+      true
+    );
+    xmlhttpRequest.send(null);
+  }
+
+  private loadDataComplete_(data: Country[]) {
+    this.dataLoaded_ = true;
+    this.countries_ = data;
+    this.start_();
+  }
+
+  /**
+   * @param {string} difficulty
+   * @private
+   */
+  private setDifficulty_(difficulty: string) {
+    this.difficulty_ = difficulty;
+
+    if (this.map_) {
+      this.setDifficultyStyle_();
+    }
+  }
+
+  private setDifficultyStyle_() {
+    const styles = {
+      easy: [
+        {
+          stylers: [{ visibility: "off" }],
+        },
+        {
+          featureType: "water",
+          stylers: [{ visibility: "on" }, { color: "#d4d4d4" }],
+        },
+        {
+          featureType: "landscape",
+          stylers: [{ visibility: "on" }, { color: "#e5e3df" }],
+        },
+        {
+          featureType: "administrative.country",
+          elementType: "labels",
+          stylers: [{ visibility: "on" }],
+        },
+        {
+          featureType: "administrative.country",
+          elementType: "geometry",
+          stylers: [{ visibility: "on" }, { weight: 1.3 }],
+        },
+      ],
+      moderate: [
+        {
+          stylers: [{ visibility: "off" }],
+        },
+        {
+          featureType: "water",
+          stylers: [{ visibility: "on" }, { color: "#d4d4d4" }],
+        },
+        {
+          featureType: "landscape",
+          stylers: [{ visibility: "on" }, { color: "#e5e3df" }],
+        },
+        {
+          featureType: "administrative.country",
+          elementType: "labels",
+          stylers: [{ visibility: "on" }],
+        },
+      ],
+      hard: [
+        {
+          stylers: [{ visibility: "off" }],
+        },
+        {
+          featureType: "water",
+          stylers: [{ visibility: "on" }, { color: "#d4d4d4" }],
+        },
+        {
+          featureType: "landscape",
+          stylers: [{ visibility: "on" }, { color: "#e5e3df" }],
+        },
+      ],
+      extreme: [
+        {
+          elementType: "geometry",
+          stylers: [{ visibility: "off" }],
+        },
+      ],
+    };
+
+    this.map_.set("styles", styles[this.difficulty_]);
+  }
+
+  private resetGame_() {
+    this.removeCountries_();
+    this.count_ = 0;
+    this.setCount_();
+    this.startClock_();
+
+    this.addRandomCountries_();
+  }
+
+  private setCount_() {
+    this.pieceDiv_.innerText = this.count_ + " / " + this.NUM_PIECES_;
+
+    if (this.count_ == this.NUM_PIECES_) {
+      this.stopClock_();
+    }
+  }
+
+  private stopClock_() {
+    window.clearInterval(this.timer_);
+  }
+
+  private startClock_() {
+    this.stopClock_();
+
+    const timeDiv = this.timeDiv_;
+
+    if (timeDiv) timeDiv.textContent = "0.0 seconds";
+    const t = new Date();
+
+    this.timer_ = window.setInterval(() => {
+      const diff = new Date().getTime() - t.getTime();
+
+      if (timeDiv) timeDiv.textContent = (diff / 1000).toFixed(2) + " seconds";
+    }, 100);
+  }
+
+  private addRandomCountries_() {
+    // Shuffle countries
+    this.countries_.sort(() => {
+      return Math.round(Math.random()) - 0.5;
+    });
+
+    const countries = this.countries_.slice(0, this.NUM_PIECES_);
+
+    for (let i = 0, country; (country = countries[i]); i++) {
+      this.addCountry_(country);
+    }
+  }
+
+  private addCountry_(country: Country) {
+    const options = {
+      strokeColor: this.START_COLOR_,
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: this.START_COLOR_,
+      fillOpacity: 0.35,
+      geodesic: true,
+      map: this.map_,
+      draggable: true,
+      zIndex: 2,
+      paths: country.start.map(google.maps.geometry.encoding.decodePath),
+    };
+
+    const poly = new google.maps.Polygon(options);
+    google.maps.event.addListener(poly, "dragend", () => {
+      this.checkPosition_(poly, country);
+    });
+
+    this.polys_.push(poly);
+  }
+
+  /**
+   * Checks that every point in the polygon is inside the bounds.
+   */
+  private boundsContainsPoly_(
+    bounds: number[][],
+    poly: google.maps.Polygon
+  ): boolean {
+    const b = new google.maps.LatLngBounds(
+      new google.maps.LatLng(bounds[0][0], bounds[0][1]),
+      new google.maps.LatLng(bounds[1][0], bounds[1][1])
+    );
+    const paths = poly.getPaths().getArray();
+
+    for (let i = 0; i < paths.length; i++) {
+      const p = paths[i].getArray();
+
+      for (let j = 0; j < p.length; j++) {
+        if (!b.contains(p[j])) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  /**
+   * Replace a poly with the correct 'end' position of the country.
+   */
+  private replacePiece_(poly: google.maps.Polygon, country: Country) {
+    const options = {
+      strokeColor: this.END_COLOR_,
+      fillColor: this.END_COLOR_,
+      draggable: false,
+      zIndex: 1,
+      paths: country.end.map(google.maps.geometry.encoding.decodePath),
+    };
+
+    poly.setOptions(options);
+    this.count_++;
+    this.setCount_();
+  }
+
+  private checkPosition_(poly: google.maps.Polygon, country: Country) {
+    if (this.boundsContainsPoly_(country.bounds, poly)) {
+      this.replacePiece_(poly, country);
+    }
+  }
+
+  private start_() {
+    this.setDifficultyStyle_();
+    this.resetGame_();
+  }
+
+  private removeCountries_() {
+    for (let i = 0, poly; (poly = this.polys_[i]); i++) {
+      poly.setMap(null);
+    }
+
+    this.polys_ = [];
+  }
+}
 
 function initMap(): void {
-  // Create a map. Use the Gall-Peters map type.
   const map = new google.maps.Map(
     document.getElementById("map") as HTMLElement,
     {
-      zoom: 0,
-      center: { lat: 0, lng: 0 },
-      mapTypeControl: false,
+      disableDefaultUI: true,
+      center: { lat: 10, lng: 60 },
+      zoom: 2,
     }
   );
 
-  initGallPeters();
-  map.mapTypes.set("gallPeters", gallPetersMapType);
-  map.setMapTypeId("gallPeters");
-
-  // Show the lat and lng under the mouse cursor.
-  const coordsDiv = document.getElementById("coords") as HTMLElement;
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(coordsDiv);
-  map.addListener("mousemove", (event: google.maps.MapMouseEvent) => {
-    coordsDiv.textContent =
-      "lat: " +
-      Math.round(event.latLng!.lat()) +
-      ", " +
-      "lng: " +
-      Math.round(event.latLng!.lng());
-  });
-
-  // Add some markers to the map.
-  map.data.setStyle((feature) => {
-    return {
-      title: feature.getProperty("name"),
-      optimized: false,
-    };
-  });
-  map.data.addGeoJson(cities);
+  new PuzzleDemo(map);
 }
-
-let gallPetersMapType;
-
-function initGallPeters() {
-  const GALL_PETERS_RANGE_X = 800;
-  const GALL_PETERS_RANGE_Y = 512;
-
-  // Fetch Gall-Peters tiles stored locally on our server.
-  gallPetersMapType = new google.maps.ImageMapType({
-    getTileUrl: function (coord, zoom) {
-      const scale = 1 << zoom;
-
-      // Wrap tiles horizontally.
-      const x = ((coord.x % scale) + scale) % scale;
-
-      // Don't wrap tiles vertically.
-      const y = coord.y;
-
-      if (y < 0 || y >= scale) return "";
-
-      return (
-        "https://developers.google.com/maps/documentation/" +
-        "javascript/examples/full/images/gall-peters_" +
-        zoom +
-        "_" +
-        x +
-        "_" +
-        y +
-        ".png"
-      );
-    },
-    tileSize: new google.maps.Size(GALL_PETERS_RANGE_X, GALL_PETERS_RANGE_Y),
-    minZoom: 0,
-    maxZoom: 1,
-    name: "Gall-Peters",
-  });
-
-  // Describe the Gall-Peters projection used by these tiles.
-  gallPetersMapType.projection = {
-    fromLatLngToPoint: function (latLng) {
-      const latRadians = (latLng.lat() * Math.PI) / 180;
-      return new google.maps.Point(
-        GALL_PETERS_RANGE_X * (0.5 + latLng.lng() / 360),
-        GALL_PETERS_RANGE_Y * (0.5 - 0.5 * Math.sin(latRadians))
-      );
-    },
-    fromPointToLatLng: function (point, noWrap) {
-      const x = point.x / GALL_PETERS_RANGE_X;
-      const y = Math.max(0, Math.min(1, point.y / GALL_PETERS_RANGE_Y));
-
-      return new google.maps.LatLng(
-        (Math.asin(1 - 2 * y) * 180) / Math.PI,
-        -180 + 360 * x,
-        noWrap
-      );
-    },
-  };
-}
-
-// GeoJSON, describing the locations and names of some cities.
-const cities = {
-  type: "FeatureCollection",
-  features: [
-    {
-      type: "Feature",
-      geometry: { type: "Point", coordinates: [-87.65, 41.85] },
-      properties: { name: "Chicago" },
-    },
-    {
-      type: "Feature",
-      geometry: { type: "Point", coordinates: [-149.9, 61.218] },
-      properties: { name: "Anchorage" },
-    },
-    {
-      type: "Feature",
-      geometry: { type: "Point", coordinates: [-99.127, 19.427] },
-      properties: { name: "Mexico City" },
-    },
-    {
-      type: "Feature",
-      geometry: { type: "Point", coordinates: [-0.126, 51.5] },
-      properties: { name: "London" },
-    },
-    {
-      type: "Feature",
-      geometry: { type: "Point", coordinates: [28.045, -26.201] },
-      properties: { name: "Johannesburg" },
-    },
-    {
-      type: "Feature",
-      geometry: { type: "Point", coordinates: [15.322, -4.325] },
-      properties: { name: "Kinshasa" },
-    },
-    {
-      type: "Feature",
-      geometry: { type: "Point", coordinates: [151.207, -33.867] },
-      properties: { name: "Sydney" },
-    },
-    {
-      type: "Feature",
-      geometry: { type: "Point", coordinates: [0, 0] },
-      properties: { name: "0°N 0°E" },
-    },
-  ],
-};
 export { initMap };
