fc2862febbedbd0f19a1454d7787f1d6db1bcffc
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:34:41 +0000

    chore: sync control-replacement [skip-ci]

--- src/index.ts
+++ src/index.ts
@@ -15,59 +15,137 @@
  */
 /* eslint-disable no-undef, @typescript-eslint/no-unused-vars, no-unused-vars */
 import "./style.css";
-/**
- * Creates a series of custom controls to demonstrate positioning
- * of controls within a map.
- */
 
-/**
- * MakeControl adds a control to the map.
- * This constructor takes the controlDIV name and label text as arguments.
- */
-function MakeControl(controlDiv: HTMLElement, label: string) {
-  // Set up the control border.
-  const controlUI = document.createElement("div");
-  controlUI.title = label;
-  controlUI.className = "controlUI";
-  controlDiv.appendChild(controlUI);
+declare global {
+  interface Document {
+    mozCancelFullScreen?: () => Promise<void>;
+    msExitFullscreen?: () => Promise<void>;
+    webkitExitFullscreen?: () => Promise<void>;
+    mozFullScreenElement?: Element;
+    msFullscreenElement?: Element;
+    webkitFullscreenElement?: Element;
+    onwebkitfullscreenchange?: any;
+    onmsfullscreenchange?: any;
+    onmozfullscreenchange?: any;
+  }
 
-  // Set up the inner control.
-  const controlText = document.createElement("div");
-  controlText.innerHTML = label;
-  controlText.className = "controlText";
-  controlUI.appendChild(controlText);
-}
-
-function initialize() {
-  const mapDiv = document.getElementById("map") as HTMLElement;
-  const mapOptions = {
-    zoom: 11,
-    center: { lat: 47.46, lng: -122.52 },
-    disableDefaultUI: true,
-  };
-  const map = new google.maps.Map(mapDiv, mapOptions);
-
-  const controlText = [
-    ["TOP_LEFT", google.maps.ControlPosition.TOP_LEFT],
-    ["LEFT_TOP", google.maps.ControlPosition.LEFT_TOP],
-    ["TOP_CENTER", google.maps.ControlPosition.TOP_CENTER],
-    ["TOP_RIGHT", google.maps.ControlPosition.TOP_RIGHT],
-    ["RIGHT_TOP", google.maps.ControlPosition.RIGHT_TOP],
-    ["LEFT_CENTER", google.maps.ControlPosition.LEFT_CENTER],
-    ["RIGHT_CENTER", google.maps.ControlPosition.RIGHT_CENTER],
-    ["BOTTOM_LEFT", google.maps.ControlPosition.BOTTOM_LEFT],
-    ["LEFT_BOTTOM", google.maps.ControlPosition.LEFT_BOTTOM],
-    ["BOTTOM_CENTER", google.maps.ControlPosition.BOTTOM_CENTER],
-    ["BOTTOM_RIGHT", google.maps.ControlPosition.BOTTOM_RIGHT],
-    ["RIGHT_BOTTOM", google.maps.ControlPosition.RIGHT_BOTTOM],
-  ];
-
-  for (let i = 0; i < controlText.length; i++) {
-    const divLabel = controlText[i][0] as string;
-    const divName = document.createElement("div");
-    MakeControl(divName, divLabel);
-    // @ts-ignore
-    map.controls[controlText[i][1]].push(divName);
+  interface HTMLElement {
+    msRequestFullScreen?: () => Promise<void>;
+    mozRequestFullScreen?: () => Promise<void>;
+    webkitRequestFullScreen?: () => Promise<void>;
   }
 }
-export { initialize };
+
+let map: google.maps.Map;
+
+function initMap(): void {
+  map = new google.maps.Map(document.querySelector("#map") as HTMLElement, {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+    disableDefaultUI: true,
+  });
+
+  initZoomControl(map);
+  initMapTypeControl(map);
+  initFullscreenControl(map);
+}
+
+function initZoomControl(map: google.maps.Map) {
+  (document.querySelector(".zoom-control-in") as HTMLElement).onclick =
+    function () {
+      map.setZoom(map.getZoom()! + 1);
+    };
+
+  (document.querySelector(".zoom-control-out") as HTMLElement).onclick =
+    function () {
+      map.setZoom(map.getZoom()! - 1);
+    };
+  map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(
+    document.querySelector(".zoom-control") as HTMLElement
+  );
+}
+
+function initMapTypeControl(map: google.maps.Map) {
+  const mapTypeControlDiv = document.querySelector(
+    ".maptype-control"
+  ) as HTMLElement;
+
+  (document.querySelector(".maptype-control-map") as HTMLElement).onclick =
+    function () {
+      mapTypeControlDiv.classList.add("maptype-control-is-map");
+      mapTypeControlDiv.classList.remove("maptype-control-is-satellite");
+      map.setMapTypeId("roadmap");
+    };
+
+  (
+    document.querySelector(".maptype-control-satellite") as HTMLElement
+  ).onclick = function () {
+    mapTypeControlDiv.classList.remove("maptype-control-is-map");
+    mapTypeControlDiv.classList.add("maptype-control-is-satellite");
+    map.setMapTypeId("hybrid");
+  };
+
+  map.controls[google.maps.ControlPosition.LEFT_TOP].push(mapTypeControlDiv);
+}
+
+function initFullscreenControl(map: google.maps.Map) {
+  const elementToSendFullscreen = map.getDiv().firstChild as HTMLElement;
+  const fullscreenControl = document.querySelector(
+    ".fullscreen-control"
+  ) as HTMLElement;
+  map.controls[google.maps.ControlPosition.RIGHT_TOP].push(fullscreenControl);
+
+  fullscreenControl.onclick = function () {
+    if (isFullscreen(elementToSendFullscreen)) {
+      exitFullscreen();
+    } else {
+      requestFullscreen(elementToSendFullscreen);
+    }
+  };
+
+  document.onwebkitfullscreenchange =
+    document.onmsfullscreenchange =
+    document.onmozfullscreenchange =
+    document.onfullscreenchange =
+      function () {
+        if (isFullscreen(elementToSendFullscreen)) {
+          fullscreenControl.classList.add("is-fullscreen");
+        } else {
+          fullscreenControl.classList.remove("is-fullscreen");
+        }
+      };
+}
+
+function isFullscreen(element: HTMLElement) {
+  return (
+    (document.fullscreenElement ||
+      document.webkitFullscreenElement ||
+      document.mozFullScreenElement ||
+      document.msFullscreenElement) == element
+  );
+}
+
+function requestFullscreen(element: HTMLElement) {
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.webkitRequestFullScreen) {
+    element.webkitRequestFullScreen();
+  } else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if (element.msRequestFullScreen) {
+    element.msRequestFullScreen();
+  }
+}
+
+function exitFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+}
+export { initMap };
