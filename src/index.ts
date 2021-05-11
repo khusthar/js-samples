78f17a9a593917ac0597b66235ac82efdce2788b
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:47:29 +0000

    chore: sync programmatic-load-button [skip-ci]

--- src/index.ts
+++ src/index.ts
@@ -18,20 +18,31 @@ import "./style.css";
 
 let map: google.maps.Map;
 
-// @ts-ignore google.maps.plugins
-const Loader = google.maps.plugins.loader.Loader;
+const center = { lat: 41.90476224706472, lng: 12.49822074385094 };
+const zoom = 14;
+const url = "https://maps.googleapis.com/maps/api/staticmap";
 
-const additionalOptions = {};
-const loader = new Loader({
+// @ts-ignore google.maps.plugins
+const loader = new google.maps.plugins.loader.Loader({
   apiKey: "YOUR_API_KEY",
   version: "weekly",
-  ...additionalOptions,
 });
 
-loader.load().then(() => {
-  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+document.addEventListener("DOMContentLoaded", () => {
+  const wrapper = document.getElementById("wrapper") as HTMLButtonElement;
+
+  wrapper.style.backgroundImage = `url(${url}?center=${center.lat},${center.lng}&zoom=${zoom}&scale=2&size=${wrapper.clientWidth}x${wrapper.clientHeight}&key=YOUR_API_KEY)`;
+
+  wrapper.addEventListener("click", () => {
+    wrapper.remove();
+
+    loader.load().then(() => {
+      map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+        center,
+        zoom,
+      });
+    });
   });
 });
+
 export { map };
