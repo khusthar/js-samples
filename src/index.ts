9b0575a7d5be6ef86798677ecfc65eca46b7887d
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:47:34 +0000

    chore: sync rectangle-event [skip-ci]

--- src/index.ts
+++ src/index.ts
@@ -16,33 +16,65 @@
 /* eslint-disable no-undef, @typescript-eslint/no-unused-vars, no-unused-vars */
 import "./style.css";
 
+// This example adds a user-editable rectangle to the map.
+// When the user changes the bounds of the rectangle,
+// an info window pops up displaying the new bounds.
+
+let rectangle: google.maps.Rectangle;
 let map: google.maps.Map;
 
-const center = { lat: 41.90476224706472, lng: 12.49822074385094 };
-const zoom = 14;
-const url = "https://maps.googleapis.com/maps/api/staticmap";
+let infoWindow: google.maps.InfoWindow;
 
-// @ts-ignore google.maps.plugins
-const loader = new google.maps.plugins.loader.Loader({
-  apiKey: "YOUR_API_KEY",
-  version: "weekly",
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  const wrapper = document.getElementById("wrapper") as HTMLButtonElement;
-
-  wrapper.style.backgroundImage = `url(${url}?center=${center.lat},${center.lng}&zoom=${zoom}&scale=2&size=${wrapper.clientWidth}x${wrapper.clientHeight}&key=YOUR_API_KEY)`;
-
-  wrapper.addEventListener("click", () => {
-    wrapper.remove();
-
-    loader.load().then(() => {
-      map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-        center,
-        zoom,
-      });
-    });
+function initMap(): void {
+  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+    center: { lat: 44.5452, lng: -78.5389 },
+    zoom: 9,
   });
-});
 
-export { map };
+  const bounds = {
+    north: 44.599,
+    south: 44.49,
+    east: -78.443,
+    west: -78.649,
+  };
+
+  // Define the rectangle and set its editable property to true.
+  rectangle = new google.maps.Rectangle({
+    bounds: bounds,
+    editable: true,
+    draggable: true,
+  });
+
+  rectangle.setMap(map);
+
+  // Add an event listener on the rectangle.
+  rectangle.addListener("bounds_changed", showNewRect);
+
+  // Define an info window on the map.
+  infoWindow = new google.maps.InfoWindow();
+}
+
+/** Show the new coordinates for the rectangle in an info window. */
+function showNewRect() {
+  const ne = rectangle.getBounds()!.getNorthEast();
+  const sw = rectangle.getBounds()!.getSouthWest();
+
+  const contentString =
+    "<b>Rectangle moved.</b><br>" +
+    "New north-east corner: " +
+    ne.lat() +
+    ", " +
+    ne.lng() +
+    "<br>" +
+    "New south-west corner: " +
+    sw.lat() +
+    ", " +
+    sw.lng();
+
+  // Set the info window's content and position.
+  infoWindow.setContent(contentString);
+  infoWindow.setPosition(ne);
+
+  infoWindow.open(map);
+}
+export { initMap };
