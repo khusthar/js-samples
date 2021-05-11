bca80a56e8b719b07d9c55c989b3c4931b8a7b19
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:38:07 +0000

    chore: sync geocoding-reverse [skip-ci]

--- src/index.ts
+++ src/index.ts
@@ -23,21 +23,53 @@ function initMap(): void {
     document.getElementById("map") as HTMLElement,
     {
       zoom: 8,
+      center: { lat: 40.731, lng: -73.997 },
     }
   );
   const geocoder = new google.maps.Geocoder();
-  geocoder.geocode({ address: "Toledo" }, (results, status) => {
-    if (status === "OK") {
-      map.setCenter(results[0].geometry.location);
-      new google.maps.Marker({
-        map,
-        position: results[0].geometry.location,
-      });
-    } else {
-      window.alert(
-        "Geocode was not successful for the following reason: " + status
-      );
+  const infowindow = new google.maps.InfoWindow();
+
+  (document.getElementById("submit") as HTMLElement).addEventListener(
+    "click",
+    () => {
+      geocodeLatLng(geocoder, map, infowindow);
     }
-  });
+  );
+}
+
+function geocodeLatLng(
+  geocoder: google.maps.Geocoder,
+  map: google.maps.Map,
+  infowindow: google.maps.InfoWindow
+) {
+  const input = (document.getElementById("latlng") as HTMLInputElement).value;
+  const latlngStr = input.split(",", 2);
+  const latlng = {
+    lat: parseFloat(latlngStr[0]),
+    lng: parseFloat(latlngStr[1]),
+  };
+  geocoder.geocode(
+    { location: latlng },
+    (
+      results: google.maps.GeocoderResult[],
+      status: google.maps.GeocoderStatus
+    ) => {
+      if (status === "OK") {
+        if (results[0]) {
+          map.setZoom(11);
+          const marker = new google.maps.Marker({
+            position: latlng,
+            map: map,
+          });
+          infowindow.setContent(results[0].formatted_address);
+          infowindow.open(map, marker);
+        } else {
+          window.alert("No results found");
+        }
+      } else {
+        window.alert("Geocoder failed due to: " + status);
+      }
+    }
+  );
 }
 export { initMap };
