9e93acdaf8401c578d1c922a6fc75d043e50ef59
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:48:08 +0000

    chore: sync streetview-custom-simple [skip-ci]

--- CLOUD_SHELL_INSTRUCTIONS.md
+++ CLOUD_SHELL_INSTRUCTIONS.md
@@ -1,11 +1,11 @@
-# Run the `streetview-controls` sample in Google Cloud Shell
+# Run the `streetview-custom-simple` sample in Google Cloud Shell
 
 <walkthrough-tutorial-duration duration="10"/>
 
 ## Introduction
 
 This tutorial will walk through the process of running a web application from
-the `streetview-controls` sample using TypeScript, Babel, and Webpack.
+the `streetview-custom-simple` sample using TypeScript, Babel, and Webpack.
 
 For feedback related to this sample, please open a new issue on
 [GitHub](https://github.com/googlemaps/js-samples/issues).
--- src/index.ts
+++ src/index.ts
@@ -16,20 +16,62 @@
 /* eslint-disable no-undef, @typescript-eslint/no-unused-vars, no-unused-vars */
 import "./style.css";
 
+// @ts-nocheck TODO(jpoehnelt) remove when fixed
+
 function initPano() {
-  // Note: constructed panorama objects have visible: true
-  // set by default.
+  // Set up Street View and initially set it visible. Register the
+  // custom panorama provider function. Set the StreetView to display
+  // the custom panorama 'reception' which we check for below.
   const panorama = new google.maps.StreetViewPanorama(
     document.getElementById("map") as HTMLElement,
-    {
-      position: { lat: 42.345573, lng: -71.098326 },
-      addressControlOptions: {
-        position: google.maps.ControlPosition.BOTTOM_CENTER,
-      },
-      linksControl: false,
-      panControl: false,
-      enableCloseButton: false,
-    }
+    { pano: "reception", visible: true }
   );
+  panorama.registerPanoProvider(getCustomPanorama);
+}
+
+// Return a pano image given the panoID.
+function getCustomPanoramaTileUrl(
+  pano: string,
+  zoom: number,
+  tileX: number,
+  tileY: number
+): string {
+  return (
+    "https://developers.google.com/maps/documentation/javascript/examples/full/images/" +
+    "panoReception1024-" +
+    zoom +
+    "-" +
+    tileX +
+    "-" +
+    tileY +
+    ".jpg"
+  );
+}
+
+// Construct the appropriate StreetViewPanoramaData given
+// the passed pano IDs.
+function getCustomPanorama(pano: string): google.maps.StreetViewPanoramaData {
+  if (pano === "reception") {
+    return {
+      location: {
+        pano: "reception",
+        description: "Google Sydney - Reception",
+      },
+      links: [],
+      // The text for the copyright control.
+      copyright: "Imagery (c) 2010 Google",
+      // The definition of the tiles for this panorama.
+      tiles: {
+        tileSize: new google.maps.Size(1024, 512),
+        worldSize: new google.maps.Size(2048, 1024),
+        // The heading in degrees at the origin of the panorama
+        // tile set.
+        centerHeading: 105,
+        getTileUrl: getCustomPanoramaTileUrl,
+      },
+    };
+  }
+  // @ts-ignore TODO(jpoehnelt) fix typings
+  return null;
 }
 export { initPano };
