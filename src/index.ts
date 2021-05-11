9e7d394f4a69c82d3acbd0397aa33529789b2280
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:38:49 +0000

    chore: sync infowindow-simple [skip-ci]

--- src/index.ts
+++ src/index.ts
@@ -16,23 +16,52 @@
 /* eslint-disable no-undef, @typescript-eslint/no-unused-vars, no-unused-vars */
 import "./style.css";
 
-// This example adds a marker to indicate the position of Bondi Beach in Sydney,
-// Australia.
+// This example displays a marker at the center of Australia.
+// When the user clicks the marker, an info window opens.
+
 function initMap(): void {
+  const uluru = { lat: -25.363, lng: 131.044 };
   const map = new google.maps.Map(
     document.getElementById("map") as HTMLElement,
     {
       zoom: 4,
-      center: { lat: -33, lng: 151 },
+      center: uluru,
     }
   );
 
-  const image =
-    "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
-  const beachMarker = new google.maps.Marker({
-    position: { lat: -33.89, lng: 151.274 },
+  const contentString =
+    '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+    '<div id="bodyContent">' +
+    "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+    "sandstone rock formation in the southern part of the " +
+    "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+    "south west of the nearest large town, Alice Springs; 450&#160;km " +
+    "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
+    "features of the Uluru - Kata Tjuta National Park. Uluru is " +
+    "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
+    "Aboriginal people of the area. It has many springs, waterholes, " +
+    "rock caves and ancient paintings. Uluru is listed as a World " +
+    "Heritage Site.</p>" +
+    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+    "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
+    "(last visited June 22, 2009).</p>" +
+    "</div>" +
+    "</div>";
+
+  const infowindow = new google.maps.InfoWindow({
+    content: contentString,
+  });
+
+  const marker = new google.maps.Marker({
+    position: uluru,
     map,
-    icon: image,
+    title: "Uluru (Ayers Rock)",
+  });
+  marker.addListener("click", () => {
+    infowindow.open(map, marker);
   });
 }
 export { initMap };
