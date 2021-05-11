43d24c502735b555fcbec49d71ac42b60237eb7f
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:47:55 +0000

    chore: sync store-locator [skip-ci]

--- src/index.ts
+++ src/index.ts
@@ -15,101 +15,250 @@
  */
 /* eslint-disable no-undef, @typescript-eslint/no-unused-vars, no-unused-vars */
 import "./style.css";
-let mapLeft: google.maps.Map, mapRight: google.maps.Map;
+let map: google.maps.Map;
+let autocomplete: google.maps.places.Autocomplete;
+let autocompleteInput: HTMLInputElement;
+let distanceMatrixService: google.maps.DistanceMatrixService;
+let progress: any;
+let isUpdateInProgress = false;
 
+interface Store {
+  name: string;
+  address?: string;
+  location: google.maps.LatLngLiteral;
+  distance?: number;
+  travelDistance?: number;
+  travelDistanceText?: string;
+  travelDuration?: number;
+  travelDurationText?: string;
+}
+
+const stores: Store[] = [];
+
+// Initialize and add the map
 function initMap(): void {
-  const mapOptions = {
-    center: { lat: 44.5250489, lng: -110.83819 },
-    zoom: 18,
-    scaleControl: false,
-    streetViewControl: false,
-  };
+  distanceMatrixService = new google.maps.DistanceMatrixService();
 
-  // instantiate the map on the left with control positioning
-  mapLeft = new google.maps.Map(
-    document.getElementById("map-left") as HTMLElement,
-    {
-      ...mapOptions,
-      mapTypeId: "satellite",
-      tilt: 0, // at high zoom levels we need to maintain the same projection
-      fullscreenControlOptions: {
-        position: google.maps.ControlPosition.LEFT_BOTTOM,
-      },
-      mapTypeControlOptions: {
-        position: google.maps.ControlPosition.LEFT_TOP,
-      },
-      zoomControlOptions: {
-        position: google.maps.ControlPosition.LEFT_BOTTOM,
-      },
-    }
+  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+    center: { lat: 39.79, lng: -104.98 },
+    zoom: 10,
+  });
+
+  // @ts-ignore
+  new mdc.textField.MDCTextField(
+    document.querySelector(".mdc-text-field") as HTMLInputElement
   );
 
-  // instantiate the map on the right with control positioning
-  mapRight = new google.maps.Map(
-    document.getElementById("map-right") as HTMLElement,
-    {
-      ...mapOptions,
-      fullscreenControlOptions: {
-        position: google.maps.ControlPosition.RIGHT_BOTTOM,
-      },
-      mapTypeControlOptions: {
-        position: google.maps.ControlPosition.RIGHT_TOP,
-      },
-      zoomControlOptions: {
-        position: google.maps.ControlPosition.RIGHT_BOTTOM,
-      },
-    }
-  );
+  autocompleteInput = document.getElementById(
+    "search-input"
+  ) as HTMLInputElement;
+  autocomplete = new google.maps.places.Autocomplete(autocompleteInput, {});
+  autocomplete.addListener("place_changed", placeChanged);
+  autocomplete.bindTo("bounds", map); // bias to map viewport
 
-  // helper function to keep maps in sync
-  function sync(...maps: google.maps.Map[]) {
-    let center: google.maps.LatLng, zoom: number;
+  fetch(
+    "https://carto.nationalmap.gov/arcgis/rest/services/structures/MapServer/23/query?where=STATE%3D%27CO%27&returnGeometry=true&outSR=4326&f=pjson"
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      const features: {
+        attributes: { NAME: string };
+        geometry: { x: number; y: number };
+      }[] = data.features;
 
-    function update(changedMap) {
-      maps.forEach((m) => {
-        if (m === changedMap) {
-          return;
+      const markers: google.maps.Marker[] = [];
+
+      features.forEach(
+        ({ attributes: { NAME: name }, geometry: { x: lng, y: lat } }) => {
+          stores.push({ name, location: { lat, lng }, address: "" });
+          const marker = new google.maps.Marker({ position: { lat, lng } });
+          marker.addListener("click", () => {
+            update(new google.maps.LatLng({ lat, lng }));
+          });
+          markers.push(marker);
         }
-        m.setCenter(center);
-        m.setZoom(zoom);
-      });
-    }
+      );
 
-    maps.forEach((m) => {
-      m.addListener("bounds_changed", () => {
-        const changedCenter = m.getCenter()!;
-        const changedZoom = m.getZoom()!;
-
-        if (changedCenter !== center || changedZoom !== zoom) {
-          center = changedCenter;
-          zoom = changedZoom;
-          update(m);
-        }
+      // @ts-ignore
+      new MarkerClusterer(map, markers, {
+        imagePath:
+          "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
       });
+
+      progress.done();
+
+      update(map.getCenter()!);
     });
-  }
 
-  sync(mapLeft, mapRight);
+  (document.getElementById("near-me") as HTMLButtonElement).addEventListener(
+    "click",
+    () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          ({ coords: { latitude: lat, longitude: lng } }) => {
+            update(new google.maps.LatLng({ lat, lng }));
+          }
+        );
+      }
+    }
+  );
 
-  function handleContainerResize() {
-    const width = (document.getElementById("container") as HTMLElement)
-      .offsetWidth;
-    (
-      document.getElementById("map-left") as HTMLElement
-    ).style.width = `${width}px`;
-    (
-      document.getElementById("map-right") as HTMLElement
-    ).style.width = `${width}px`;
-  }
+  (document.getElementById("refresh") as HTMLButtonElement).addEventListener(
+    "click",
+    () => {
+      update(map.getCenter()!);
+    }
+  );
+}
 
-  // trigger to set map container size since using absolute
-  handleContainerResize();
-  // add event listener
-  window.addEventListener("resize", handleContainerResize);
+function renderCards(stores: Store[]): void {
+  const cardsDiv = document.getElementById("cards") as HTMLElement;
+  cardsDiv.innerHTML = "";
 
-  //@ts-ignore
-  Split(["#left", "#right"], {
-    sizes: [50, 50],
+  stores
+    .slice(0, 25)
+    .forEach(
+      ({ name, location, address, travelDistanceText, travelDurationText }) => {
+        const card = document.createElement("div");
+        card.classList.add("mdc-card", "mdc-card--outlined");
+
+        card.innerHTML = `
+<div class="mdc-card__primary-action">
+  <div id="card-header">
+    <h2 class="mdc-typography--headline6">${name}</h2>
+  </div>
+</div>
+<div id="card-body">
+  </div>
+<div class="mdc-card__actions">
+  <a class="mdc-button mdc-card__action mdc-card__action--button" 
+    target="_blank" href="https://maps.google.com?q=${
+      address ? address : name
+    }">
+    <div class="mdc-button__ripple"></div>
+    <span class="mdc-button__label">Directions</span>
+  </a>
+  <button class="mdc-button mdc-card__action mdc-card__action--button">
+    <div class="mdc-button__ripple"></div>
+    <span class="mdc-button__label">More Information</span>
+  </button>
+</div>`;
+
+        const cardBody = card.querySelector("#card-body") as HTMLElement;
+
+        if (address) {
+          cardBody.innerHTML = `<h2 class="mdc-typography--body1">${address}</h2>`;
+        }
+
+        if (travelDistanceText) {
+          cardBody.innerHTML += `<h2 class="mdc-typography--body2">${travelDistanceText}, ${travelDurationText}</h2>`;
+        }
+
+        (
+          card.querySelector(".mdc-card__primary-action") as HTMLElement
+        ).addEventListener("click", () => {
+          map.panTo(location);
+        });
+
+        cardsDiv.appendChild(card);
+      }
+    );
+  cardsDiv.scrollTo(0, 0);
+}
+
+function getDistances(
+  place: google.maps.Place | google.maps.LatLng
+): Promise<google.maps.DistanceMatrixResponse> {
+  const origins = [place];
+
+  return new Promise((resolve, reject) => {
+    const callback = (
+      response: google.maps.DistanceMatrixResponse | null,
+      status: google.maps.DistanceMatrixStatus
+    ) => {
+      if (status === google.maps.DistanceMatrixStatus.OK && response) {
+        resolve(response);
+      } else {
+        reject(status);
+      }
+    };
+
+    distanceMatrixService.getDistanceMatrix(
+      {
+        origins,
+        destinations: stores.slice(0, 25).map((store) => store.location),
+        travelMode: google.maps.TravelMode.DRIVING,
+        unitSystem: google.maps.UnitSystem.IMPERIAL,
+      },
+      callback
+    );
   });
 }
+
+function placeChanged() {
+  autocompleteInput.disabled = true;
+  const placeResult = autocomplete.getPlace();
+  const location = (placeResult.geometry as google.maps.places.PlaceGeometry)
+    .location;
+
+  update(location);
+}
+
+function update(location?: google.maps.LatLng) {
+  if (!location) {
+    return;
+  }
+
+  if (isUpdateInProgress) {
+    alert("Update in progress, please try again.");
+    return;
+  }
+
+  autocompleteInput.disabled = true;
+  isUpdateInProgress = true;
+  map.setCenter(location);
+
+  // reset values
+  stores.forEach((store) => {
+    delete store.travelDistance;
+    delete store.travelDistanceText;
+    delete store.travelDuration;
+    delete store.travelDurationText;
+  });
+
+  // sort by distance
+  stores.sort((a, b) => {
+    return (
+      google.maps.geometry.spherical.computeDistanceBetween(
+        new google.maps.LatLng(a.location),
+        location
+      ) -
+      google.maps.geometry.spherical.computeDistanceBetween(
+        new google.maps.LatLng(b.location),
+        location
+      )
+    );
+  });
+
+  getDistances(location)
+    .then((response) => {
+      for (let i = 0; i < response.rows[0].elements.length; i++) {
+        stores[i].address = response.destinationAddresses[i];
+        stores[i].travelDistance = response.rows[0].elements[i].distance.value;
+        stores[i].travelDistanceText =
+          response.rows[0].elements[i].distance.text;
+        stores[i].travelDuration = response.rows[0].elements[i].duration.value;
+        stores[i].travelDurationText =
+          response.rows[0].elements[i].duration.text;
+      }
+    })
+    .finally(() => {
+      renderCards(stores);
+      autocompleteInput.disabled = false;
+      isUpdateInProgress = false;
+    });
+}
+
 export { initMap };
