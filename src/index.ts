bd6fa335f7f2673481023b5a679f4eb073793b72
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:41:09 +0000

    chore: sync layer-kml-features [skip-ci]

--- src/index.ts
+++ src/index.ts
@@ -20,14 +20,25 @@ function initMap(): void {
   const map = new google.maps.Map(
     document.getElementById("map") as HTMLElement,
     {
-      zoom: 11,
-      center: { lat: 41.876, lng: -87.624 },
+      zoom: 12,
+      center: { lat: 37.06, lng: -95.68 },
     }
   );
 
-  const ctaLayer = new google.maps.KmlLayer({
-    url: "https://googlearchive.github.io/js-v2-samples/ggeoxml/cta.kml",
+  const kmlLayer = new google.maps.KmlLayer({
+    url: "https://raw.githubusercontent.com/googlearchive/kml-samples/gh-pages/kml/Placemark/placemark.kml",
+    suppressInfoWindows: true,
     map: map,
   });
+
+  kmlLayer.addListener("click", (kmlEvent) => {
+    const text = kmlEvent.featureData.description;
+    showInContentWindow(text);
+  });
+
+  function showInContentWindow(text: string) {
+    const sidebar = document.getElementById("sidebar") as HTMLElement;
+    sidebar.innerHTML = text;
+  }
 }
 export { initMap };
