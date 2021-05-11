89c11bd3908aa68aac26e828eae73b47051e106d
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:38:54 +0000

    chore: sync infowindow-simple-max [skip-ci]

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,7 @@ import "./style.css";
 
 // This example displays a marker at the center of Australia.
 // When the user clicks the marker, an info window opens.
+// The maximum width of the info window is set to 200 pixels.
 
 function initMap(): void {
   const uluru = { lat: -25.363, lng: 131.044 };
@@ -53,6 +54,7 @@ function initMap(): void {
 
   const infowindow = new google.maps.InfoWindow({
     content: contentString,
+    maxWidth: 200,
   });
 
   const marker = new google.maps.Marker({
