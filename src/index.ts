cb6342e848736af454da513a0d44314e91e59ca0
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:45:58 +0000

    chore: sync places-autocomplete-service [skip-ci]

--- src/index.ts
+++ src/index.ts
@@ -16,120 +16,146 @@
 /* eslint-disable no-undef, @typescript-eslint/no-unused-vars, no-unused-vars */
 import "./style.css";
 
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+let autocompleteService: google.maps.places.AutocompleteService;
+let placesService: google.maps.places.PlacesService;
 
-function initMap(): void {
-  const map = new google.maps.Map(
-    document.getElementById("map") as HTMLElement,
-    {
-      center: { lat: 50.064192, lng: -130.605469 },
-      zoom: 3,
+let requestElement: HTMLPreElement;
+let responseElement: HTMLPreElement;
+let inputElement: HTMLInputElement;
+let biasToMapSwitchElement: HTMLInputElement;
+let autocompleteTypeElement: HTMLInputElement;
+
+let map: google.maps.Map;
+
+const debounce = <F extends (...args: any[]) => void>(delay: number, fn: F) => {
+  let timeout = 0;
+
+  const debounced = (...args: any[]) => {
+    window.clearTimeout(timeout);
+    timeout = window.setTimeout(() => fn(...args), delay);
+  };
+
+  return debounced as (...args: Parameters<F>) => void;
+};
+
+const initialize = (): void => {
+  map = new google.maps.Map(document.getElementById("map")!, {
+    center: { lat: 47.609414458375674, lng: -122.33897030353548 },
+    zoom: 12,
+  });
+
+  autocompleteService = new google.maps.places.AutocompleteService();
+  placesService = new google.maps.places.PlacesService(map);
+
+  inputElement = document.getElementById("input") as HTMLInputElement;
+  requestElement = document.getElementById("request") as HTMLPreElement;
+  responseElement = document.getElementById("response") as HTMLPreElement;
+  biasToMapSwitchElement = document.getElementById(
+    "bias-to-map"
+  ) as HTMLInputElement;
+  autocompleteTypeElement = document.getElementById(
+    "autocomplete-type"
+  ) as HTMLInputElement;
+
+  const autocompleteTypeListElement = document.getElementById(
+    "autocomplete-type-list"
+  ) as HTMLUListElement;
+  ["", "establishment", "geocode", "address", "(cities)", "(regions)"].forEach(
+    (type) => {
+      const item = document.createElement("LI");
+      item.classList.add("mdc-list-item");
+      item.setAttribute("data-value", type);
+
+      const itemText = document.createElement("SPAN") as HTMLSpanElement;
+      itemText.classList.add("mdc-list-item__text");
+      itemText.innerText = type;
+
+      item.appendChild(itemText);
+      autocompleteTypeListElement.appendChild(item);
     }
   );
-  const card = document.getElementById("pac-card") as HTMLElement;
-  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
-  const center = { lat: 50.064192, lng: -130.605469 };
-  // Create a bounding box with sides ~10km away from the center point
-  const defaultBounds = {
-    north: center.lat + 0.1,
-    south: center.lat - 0.1,
-    east: center.lng + 0.1,
-    west: center.lng - 0.1,
+
+  inputElement.addEventListener("input", inputChangeCallback);
+  biasToMapSwitchElement.addEventListener("change", inputChangeCallback);
+  map.addListener("bounds_changed", () => {
+    if (biasToMapSwitchElement.checked) {
+      inputChangeCallback();
+    }
+  });
+
+  biasToMapSwitchElement.checked = true;
+
+  initializeMaterialDesignComponents();
+  inputChangeCallback();
+};
+
+const inputChangeCallback = debounce(100, () => {
+  const request: google.maps.places.AutocompletionRequest = {
+    input: inputElement.value,
   };
-  const input = document.getElementById("pac-input") as HTMLInputElement;
-  const options = {
-    bounds: defaultBounds,
-    componentRestrictions: { country: "us" },
-    fields: ["address_components", "geometry", "icon", "name"],
-    origin: center,
-    strictBounds: false,
-    types: ["establishment"],
-  };
 
-  const autocomplete = new google.maps.places.Autocomplete(input, options);
+  const bounds = map.getBounds();
 
-  // Set initial restriction to the greater list of countries.
-  autocomplete.setComponentRestrictions({
-    country: ["us", "pr", "vi", "gu", "mp"],
-  });
-
-  const southwest = { lat: 5.6108, lng: 136.589326 };
-  const northeast = { lat: 61.179287, lng: 2.64325 };
-  const newBounds = new google.maps.LatLngBounds(southwest, northeast);
-  autocomplete.setBounds(newBounds);
-
-  const infowindow = new google.maps.InfoWindow();
-  const infowindowContent = document.getElementById(
-    "infowindow-content"
-  ) as HTMLElement;
-  infowindow.setContent(infowindowContent);
-  const marker = new google.maps.Marker({
-    map,
-    anchorPoint: new google.maps.Point(0, -29),
-  });
-
-  autocomplete.addListener("place_changed", () => {
-    infowindow.close();
-    marker.setVisible(false);
-    const place = autocomplete.getPlace();
-
-    if (!place.geometry || !place.geometry.location) {
-      // User entered the name of a Place that was not suggested and
-      // pressed the Enter key, or the Place Details request failed.
-      window.alert("No details available for input: '" + place.name + "'");
-      return;
-    }
-
-    // If the place has a geometry, then present it on a map.
-    if (place.geometry.viewport) {
-      map.fitBounds(place.geometry.viewport);
-    } else {
-      map.setCenter(place.geometry.location);
-      map.setZoom(17); // Why 17? Because it looks good.
-    }
-    marker.setPosition(place.geometry.location);
-    marker.setVisible(true);
-
-    let address = "";
-
-    if (place.address_components) {
-      address = [
-        (place.address_components[0] &&
-          place.address_components[0].short_name) ||
-          "",
-        (place.address_components[1] &&
-          place.address_components[1].short_name) ||
-          "",
-        (place.address_components[2] &&
-          place.address_components[2].short_name) ||
-          "",
-      ].join(" ");
-    }
-
-    infowindowContent.children["place-icon"].src = place.icon;
-    infowindowContent.children["place-name"].textContent = place.name;
-    infowindowContent.children["place-address"].textContent = address;
-    infowindow.open(map, marker);
-  });
-
-  // Sets a listener on a given radio button. The radio buttons specify
-  // the countries used to restrict the autocomplete search.
-  function setupClickListener(id, countries) {
-    const radioButton = document.getElementById(id) as HTMLElement;
-    radioButton.addEventListener("click", () => {
-      autocomplete.setComponentRestrictions({ country: countries });
-    });
+  if (biasToMapSwitchElement.checked && bounds) {
+    request.bounds = bounds;
   }
 
-  setupClickListener("changecountry-usa", "us");
-  setupClickListener("changecountry-usa-and-uot", [
-    "us",
-    "pr",
-    "vi",
-    "gu",
-    "mp",
-  ]);
-}
-export { initMap };
+  const selectedAutocompleteType = document.querySelector(
+    "#autocomplete-type-list > .mdc-list-item--selected"
+  );
+
+  if (
+    selectedAutocompleteType &&
+    selectedAutocompleteType.getAttribute("data-value") !== ""
+  ) {
+    request.types = [selectedAutocompleteType.getAttribute("data-value")!];
+  }
+
+  requestElement.innerText = JSON.stringify(request, null, 2);
+
+  if (!inputElement.value) {
+    return;
+  }
+
+  autocompleteService.getPlacePredictions(request, predictionsCallback);
+});
+
+const predictionsCallback = (
+  results: google.maps.places.AutocompletePrediction[] | null,
+  status: google.maps.places.PlacesServiceStatus
+) => {
+  responseElement.innerText = JSON.stringify({ results, status }, null, 2);
+};
+
+const initializeMaterialDesignComponents = () => {
+  document.querySelectorAll(".mdc-text-field").forEach(
+    (el) =>
+      // @ts-ignore
+      new mdc.textField.MDCTextField(el)
+  );
+
+  document
+    .querySelectorAll(".mdc-switch")
+    // @ts-ignore
+    .forEach((el) => new mdc.switchControl.MDCSwitch(el));
+
+  document.querySelectorAll(".mdc-select").forEach((el) =>
+    // @ts-ignore
+    new mdc.select.MDCSelect(el).listen("MDCSelect:change", inputChangeCallback)
+  );
+
+  // @ts-ignore
+  const tabBar = new mdc.tabBar.MDCTabBar(
+    document.querySelector(".mdc-tab-bar")
+  );
+  const contentElements = document.querySelectorAll(".tab-content");
+
+  tabBar.listen("MDCTabBar:activated", (event) => {
+    document
+      .querySelector(".tab-content--active")!
+      .classList.remove("tab-content--active");
+    contentElements[event.detail.index].classList.add("tab-content--active");
+  });
+};
+
+export { initialize };
