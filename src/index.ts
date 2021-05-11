577816f57a26fed3fe0167d31bafba07b5d1f420
Justin Poehnelt <jpoehnelt@google.com>
2021-05-11 20:47:45 +0000

    chore: sync rectangle-zoom [skip-ci]

--- src/index.ts
+++ src/index.ts
@@ -16,31 +16,32 @@
 /* eslint-disable no-undef, @typescript-eslint/no-unused-vars, no-unused-vars */
 import "./style.css";
 
-// This example adds a red rectangle to a map.
+// This example creates a rectangle based on the viewport
+// on any 'zoom-changed' event.
 
 function initMap(): void {
   const map = new google.maps.Map(
     document.getElementById("map") as HTMLElement,
     {
       zoom: 11,
-      center: { lat: 33.678, lng: -116.243 },
+      center: { lat: 40.74852, lng: -73.981687 },
       mapTypeId: "terrain",
     }
   );
 
-  const rectangle = new google.maps.Rectangle({
-    strokeColor: "#FF0000",
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: "#FF0000",
-    fillOpacity: 0.35,
-    map,
-    bounds: {
-      north: 33.685,
-      south: 33.671,
-      east: -116.234,
-      west: -116.251,
-    },
+  const rectangle = new google.maps.Rectangle();
+
+  map.addListener("zoom_changed", () => {
+    // Get the current bounds, which reflect the bounds before the zoom.
+    rectangle.setOptions({
+      strokeColor: "#FF0000",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#FF0000",
+      fillOpacity: 0.35,
+      map,
+      bounds: map.getBounds() as google.maps.LatLngBounds,
+    });
   });
 }
 export { initMap };
